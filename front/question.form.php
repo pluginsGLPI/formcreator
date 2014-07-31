a54c1dbd1dec30551f000d702eb6ac28d8f70cc1
Jérémy MOREAU <jmoreau@teclib.com>
2014-07-31 10:43:36 +0200

    Remove a useless redirection

--- front/question.form.php
+++ front/question.form.php
@@ -58,7 +58,6 @@ if ($plugin->isActivated("formcreator")) {
             $DB->query("UPDATE $table SET `order` = $order WHERE `id` = $id2");
          }
       }
-      Html::redirect($CFG_GLPI["root_doc"] . '/plugins/formcreator/front/form.form.php?id=' . $_POST['plugin_formcreator_forms_id']);
 
    // Return to form list
    } else {
