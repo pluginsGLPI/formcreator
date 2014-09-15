dd4ce9ded09ea256ce6eecbabdb58a1668326f6f
Jérémy MOREAU <jmoreau.dev@gmail.com>
2014-09-15 14:46:42 +0200

    Add answers classes

---
+++ inc/answer.class.php
@@ -0,0 +1,111 @@
+<?php
+class PluginFormcreatorAnswer extends CommonDBChild
+{
+   static public $itemtype = "PluginFormcreatorFormanswer";
+   static public $items_id = "plugin_formcreator_formanwers_id";
+
+   /**
+    * Check if current user have the right to create and modify requests
+    *
+    * @return boolean True if he can create and modify requests
+    */
+   public static function canCreate()
+   {
+      return true;
+   }
+
+   /**
+    * Check if current user have the right to read requests
+    *
+    * @return boolean True if he can read requests
+    */
+   public static function canView()
+   {
+      return true;
+   }
+
+   /**
+    * Returns the type name with consideration of plural
+    *
+    * @param number $nb Number of item(s)
+    * @return string Itemtype name
+    */
+   public static function getTypeName($nb = 0)
+   {
+      return _n('Answer', 'Answers', $nb, 'formcreator');
+   }
+
+   /**
+    * Prepare input datas for adding the question
+    * Check fields values and get the order for the new question
+    *
+    * @param $input datas used to add the item
+    *
+    * @return the modified $input array
+   **/
+   public function prepareInputForAdd($input)
+   {
+
+      return $input;
+   }
+
+   /**
+    * Prepare input datas for adding the question
+    * Check fields values and get the order for the new question
+    *
+    * @param $input datas used to add the item
+    *
+    * @return the modified $input array
+   **/
+   public function prepareInputForUpdate($input)
+   {
+
+      return $input;
+   }
+
+   /**
+    * Database table installation for the item type
+    *
+    * @param Migration $migration
+    * @return boolean True on success
+    */
+   public static function install(Migration $migration)
+   {
+      $obj   = new self();
+      $table = $obj->getTable();
+
+      if (!TableExists($table)) {
+         $migration->displayMessage("Installing $table");
+
+         // Create questions table
+         $query = "CREATE TABLE IF NOT EXISTS `$table` (
+                     `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                     `plugin_formcreator_formanwers_id` int(11) NOT NULL,
+                     `plugin_formcreator_question_id` int(11) NOT NULL,
+                     `answer` varchar(255) NOT NULL DEFAULT ''
+                  )
+                  ENGINE = MyISAM
+                  DEFAULT CHARACTER SET = utf8
+                  COLLATE = utf8_unicode_ci";
+         $GLOBALS['DB']->query($query) or die ($GLOBALS['DB']->error());
+      }
+
+      return true;
+   }
+
+   /**
+    * Database table uninstallation for the item type
+    *
+    * @return boolean True on success
+    */
+   public static function uninstall()
+   {
+      $obj = new self();
+      $GLOBALS['DB']->query('DROP TABLE IF EXISTS `' . $obj->getTable() . '`');
+
+      // Delete logs of the plugin
+      $GLOBALS['DB']->query('DELETE FROM `glpi_logs` WHERE itemtype = "' . __CLASS__ . '"');
+
+      return true;
+   }
+}
---
+++ inc/formanswer.class.php
@@ -0,0 +1,112 @@
+<?php
+class PluginFormcreatorFormanswer extends CommonDBChild
+{
+   static public $itemtype = "PluginFormcreatorForm";
+   static public $items_id = "plugin_formcreator_forms_id";
+
+   /**
+    * Check if current user have the right to create and modify requests
+    *
+    * @return boolean True if he can create and modify requests
+    */
+   public static function canCreate()
+   {
+      return true;
+   }
+
+   /**
+    * Check if current user have the right to read requests
+    *
+    * @return boolean True if he can read requests
+    */
+   public static function canView()
+   {
+      return true;
+   }
+
+   /**
+    * Returns the type name with consideration of plural
+    *
+    * @param number $nb Number of item(s)
+    * @return string Itemtype name
+    */
+   public static function getTypeName($nb = 0)
+   {
+      return _n('Form answer', 'Form answers', $nb, 'formcreator');
+   }
+
+   /**
+    * Prepare input datas for adding the question
+    * Check fields values and get the order for the new question
+    *
+    * @param $input datas used to add the item
+    *
+    * @return the modified $input array
+   **/
+   public function prepareInputForAdd($input)
+   {
+
+      return $input;
+   }
+
+   /**
+    * Prepare input datas for adding the question
+    * Check fields values and get the order for the new question
+    *
+    * @param $input datas used to add the item
+    *
+    * @return the modified $input array
+   **/
+   public function prepareInputForUpdate($input)
+   {
+
+      return $input;
+   }
+
+   /**
+    * Database table installation for the item type
+    *
+    * @param Migration $migration
+    * @return boolean True on success
+    */
+   public static function install(Migration $migration)
+   {
+      $obj   = new self();
+      $table = $obj->getTable();
+
+      if (!TableExists($table)) {
+         $migration->displayMessage("Installing $table");
+
+         // Create questions table
+         $query = "CREATE TABLE IF NOT EXISTS `$table` (
+                     `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                     `plugin_formcreator_forms_id` int(11) NOT NULL,
+                     `requester_id` int(11) NULL,
+                     `validator_id` int(11) NOT NULL,
+                     `status` enum('waiting', 'refused', 'accepted', 'deleted') NOT NULL DEFAULT 'waiting'
+                  )
+                  ENGINE = MyISAM
+                  DEFAULT CHARACTER SET = utf8
+                  COLLATE = utf8_unicode_ci";
+         $GLOBALS['DB']->query($query) or die ($GLOBALS['DB']->error());
+      }
+
+      return true;
+   }
+
+   /**
+    * Database table uninstallation for the item type
+    *
+    * @return boolean True on success
+    */
+   public static function uninstall()
+   {
+      $obj = new self();
+      $GLOBALS['DB']->query('DROP TABLE IF EXISTS `' . $obj->getTable() . '`');
+
+      // Delete logs of the plugin
+      $GLOBALS['DB']->query('DELETE FROM `glpi_logs` WHERE itemtype = "' . __CLASS__ . '"');
+
+      return true;
+   }
+}
