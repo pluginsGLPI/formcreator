46af194e07adcea56bd1f3bd5bbd89e333849372
Alexandre Delaunay <adelaunay@teclib.com>
2015-03-06 10:14:12 +0100

    encode also checkboxes

--- ajax/showfields.php
+++ ajax/showfields.php
@@ -2,8 +2,15 @@
 include ('../../../inc/includes.php');
 
 $currentValues  = json_decode(stripslashes($_POST['values']), true);
-foreach ($currentValues as $id => &$value) {
-   $value = str_replace("'", "&apos;", htmlentities(html_entity_decode($value)));
+
+foreach ($currentValues as &$value) {
+   if (!is_array($value)) {
+      $value = str_replace("'", "&apos;", htmlentities(html_entity_decode($value)));
+   } else {
+      foreach ($value as &$sub_value) {
+         $sub_value = str_replace("'", "&apos;", htmlentities(html_entity_decode($sub_value)));
+      }
+   }
 }
 $questionToShow = array();
 
