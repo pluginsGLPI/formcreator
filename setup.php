24c762bbe32f844b61c7e1a1977bf07392918778
Jérémy MOREAU <jmoreau@teclib.com>
2015-03-10 17:30:43 +0100

    Update plugin version for next tag

--- setup.php
+++ setup.php
@@ -7,7 +7,7 @@
 function plugin_version_formcreator ()
 {
    return array('name'       => _n('Form', 'Forms', 2, 'formcreator'),
-            'version'        => '0.85-1.2',
+            'version'        => '0.85-1.2.1',
             'author'         => '<a href="mailto:jmoreau@teclib.com">Jérémy MOREAU</a>
                                   - <a href="http://www.teclib.com">Teclib\'</a>',
             'homepage'       => 'https://github.com/TECLIB/formcreator',
