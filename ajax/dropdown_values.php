8403ac4662709cbca5243af59cb2730fb128826f
Jérémy Moreau <jmoreau@teclib.com>
2015-11-27 17:15:50 +0100

    Fix #152 : Change random id of dropdowns default value

--- ajax/dropdown_values.php
+++ ajax/dropdown_values.php
@@ -6,7 +6,7 @@ Session::checkRight("entity", UPDATE);
 if(class_exists($_REQUEST['dropdown_itemtype'])) {
    Dropdown::show($_REQUEST['dropdown_itemtype'], array(
       'name' => 'dropdown_default_value',
-      'rand' => $_REQUEST['rand'],
+      'rand' => mt_rand(),
    ));
 } else {
    echo '<select name="dropdown_dropdown_default_value<?php echo $rand; ?>">
