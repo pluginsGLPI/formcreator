1a3121788ac9e22ffdeaf47d963fc177788fb5c5
dethegeek <dethegeek@dethegeek-3060d.intra.dethegeek.eu.org>
2016-07-05 10:20:52 +0200

    link labels to radios

--- inc/form.class.php
+++ inc/form.class.php
@@ -523,8 +523,8 @@ class PluginFormcreatorForm extends CommonDBTM
 //       echo '<li><a href="#" class="alphabeticSort">'.__('Alphabetic sort', 'formcreator').'</a></li>';
 //       echo '</ul>';
       echo '<div class="plugin_formcreator_sort">';
-      echo '<label for="">'.__('Popularity sort', 'formcreator').'</label><input type="radio" class="form-control" name="sort" value="mostPopularSort" />';
-      echo '<label for="">'.__('Alphabetic sort', 'formcreator').'</label><input type="radio" class="form-control" name="sort" value="alphabeticSort" />';
+      echo '<label for="plugin_formcreator_mostPopular">'.__('Popularity sort', 'formcreator').'</label><input type="radio" class="form-control" id="plugin_formcreator_mostPopular" name="sort" value="mostPopularSort" />';
+      echo '<label for="plugin_formcreator_alphabetic">'.__('Alphabetic sort', 'formcreator').'</label><input type="radio" class="form-control" id="plugin_formcreator_alphabetic" name="sort" value="alphabeticSort" />';
       echo '</div>';
       echo '<div id="plugin_formcreator_wizard_forms">';
       echo '</div>';
