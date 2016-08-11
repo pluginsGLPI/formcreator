94ecfb0b56e260ed847afd2dbb911b23a616a876
François Legastelois <flegastelois@teclib.com>
2016-08-11 17:24:25 +0200

    use same code as CommonITILObject::prepareInputForAdd

--- setup.php
+++ setup.php
@@ -155,10 +155,13 @@ function plugin_init_formcreator ()
  */
 function plugin_formcreator_encode($string)
 {
-   $string = stripcslashes($string);
-   $string = html_entity_decode($string, ENT_QUOTES, 'UTF-8');
-   $string = str_replace('&apos;', "'", $string);
-   $string = htmlentities($string, ENT_QUOTES, 'UTF-8');
+   $string = Html::clean(Html::entity_decode_deep($string));
+   $string = preg_replace('/\\r\\n/',' ',$string);
+   $string = preg_replace('/\\n/',' ',$string);
+   $string = preg_replace('/\\\\r\\\\n/',' ',$string);
+   $string = preg_replace('/\\\\n/',' ',$string);
+   $string = Toolbox::stripslashes_deep($string);
+   $string = Toolbox::addslashes_deep($string);
    return $string;
 }
 
