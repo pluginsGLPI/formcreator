bf534e39272c8ccb5267fe79f51bf4af59053c74
btry <tbugier@teclib.com>
2016-10-01 20:22:07 +0200

    Fix regex for inclusion in install process

--- hook.php
+++ hook.php
@@ -12,7 +12,7 @@ function plugin_formcreator_install()
    // Parse inc directory
    foreach (glob(dirname(__FILE__).'/inc/*') as $filepath) {
       // Load *.class.php files and get the class name
-      if (preg_match("/inc.(.+)\.class.php/", $filepath, $matches)) {
+      if (preg_match("/inc.(.+)\.class.php$/", $filepath, $matches)) {
          $classname = 'PluginFormcreator' . ucfirst($matches[1]);
          include_once($filepath);
          // If the install method exists, load it
