f334b5082170ecde091831ecf1c94614fcbbe9fc
btry <tbugier@teclib.com>
2016-10-03 20:09:24 +0200

    fix use if defautl value for select field

--- inc/fields/select-field.class.php
+++ inc/fields/select-field.class.php
@@ -43,7 +43,7 @@ class selectField extends PluginFormcreatorField
    {
       $values = $this->getAvailableValues();
       $value  = $this->getValue();
-      return isset($values[$value]) ? $values[$value] : $value;
+      return in_array($value, $value) ? $value : $this->fields['default_values'];
    }
 
    public static function getName()
