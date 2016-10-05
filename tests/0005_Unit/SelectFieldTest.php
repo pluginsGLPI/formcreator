ed8324d0863c8c0d69d8a188fe0a3f6b3661a143
btry <tbugier@teclib.com>
2016-10-05 17:56:18 +0200

    enhance tests for select field

--- tests/0005_Unit/SelectFieldTest.php
+++ tests/0005_Unit/SelectFieldTest.php
@@ -20,7 +20,8 @@ class SelectFieldTest extends SuperAdminTestCase {
                         'show_rule'       => 'always'
                   ),
                   'data'            => null,
-                  'expectedValue'   => '1'
+                  'expectedValue'   => '1',
+                  'expectedIsValid' => true
             ),
             array(
                   'fields'          => array(
@@ -34,7 +35,8 @@ class SelectFieldTest extends SuperAdminTestCase {
                         'show_rule'       => 'always'
                   ),
                   'data'            => null,
-                  'expectedValue'   => ''
+                  'expectedValue'   => '',
+                  'expectedIsValid' => true
             ),
             array(
                   'fields'          => array(
@@ -48,8 +50,39 @@ class SelectFieldTest extends SuperAdminTestCase {
                         'show_rule'       => 'always'
                   ),
                   'data'            => null,
-                  'expectedValue'   => '3'
-            )
+                  'expectedValue'   => '3',
+                  'expectedIsValid' => true
+            ),
+             array(
+                  'fields'          => array(
+                        'fieldtype'       => 'select',
+                        'name'            => 'question',
+                        'required'        => '1',
+                        'show_empty'      => '0',
+                        'default_values'  => '',
+                        'values'          => "1\r\n2\r\n3\r\n4\r\n5\r\n6",
+                        'order'           => '1',
+                        'show_rule'       => 'always'
+                  ),
+                  'data'            => null,
+                  'expectedValue'   => '1',
+                  'expectedIsValid' => false
+            ),
+             array(
+                  'fields'          => array(
+                        'fieldtype'       => 'select',
+                        'name'            => 'question',
+                        'required'        => '1',
+                        'show_empty'      => '1',
+                        'default_values'  => '',
+                        'values'          => "1\r\n2\r\n3\r\n4\r\n5\r\n6",
+                        'order'           => '1',
+                        'show_rule'       => 'always'
+                  ),
+                  'data'            => null,
+                  'expectedValue'   => '',
+                  'expectedIsValid' => false
+            ),
       );
 
       foreach($dataset as &$data) {
@@ -62,7 +95,7 @@ class SelectFieldTest extends SuperAdminTestCase {
    /**
     * @dataProvider provider
     */
-   public function testFieldAvailableValue($fields, $data, $expectedValue, $fieldInstance) {
+   public function testFieldAvailableValue($fields, $data, $expectedValue, $expectedValidity, $fieldInstance) {
       $availableValues = $fieldInstance->getAvailableValues();
       $expectedAvaliableValues = explode("\r\n", $fields['values']);
 
@@ -76,9 +109,17 @@ class SelectFieldTest extends SuperAdminTestCase {
    /**
     * @dataProvider provider
     */
-   public function testFieldValue($fields, $data, $expectedValue, $fieldInstance) {
+   public function testFieldValue($fields, $data, $expectedValue, $expectedValidity, $fieldInstance) {
       $value = $fieldInstance->getValue();
       $this->assertEquals($expectedValue, $value);
+   }
+
+   /**
+    * @dataProvider provider
+    */
+   public function testFieldIsValid($fields, $data, $expectedValue, $expectedValidity, $fieldInstance) {
+      $isValid = $fieldInstance->isValid($fields['default_values']);
+      $this->assertEquals($expectedValidity, $isValid);
    }
 
 }
