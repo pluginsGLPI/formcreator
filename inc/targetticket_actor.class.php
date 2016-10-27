8f98c3c935ce0073092dcb5685c982aa50f8223c
btry <tbugier@teclib.com>
2016-10-27 09:36:18 +0200

    handle upgrade of the remaining enum

--- inc/targetticket_actor.class.php
+++ inc/targetticket_actor.class.php
@@ -16,18 +16,27 @@ class PluginFormcreatorTargetTicket_Actor extends CommonDBTM
       );
    }
 
+   static function getEnumRole() {
+      return array(
+            'requester'          => __("Requester"),
+            'observer'           => __("Observer"),
+            'assigned'           => __("Assigned to"),
+      );
+   }
+
    public static function install(Migration $migration)
    {
       global $DB;
 
       $enum_actor_type = "'".implode("', '", array_keys(self::getEnumActorType()))."'";
+      $enum_actor_role = "'".implode("', '", array_keys(self::getEnumRole()))."'";
 
       $table = getTableForItemType(__CLASS__);
       if (!TableExists($table)) {
          $query = "CREATE TABLE IF NOT EXISTS `$table` (
                     `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
                     `plugin_formcreator_targettickets_id` int(11) NOT NULL,
-                    `actor_role` enum('requester','observer','assigned') NOT NULL,
+                    `actor_role` enum($enum_actor_role) NOT NULL,
                     `actor_type` enum($enum_actor_type) NOT NULL,
                     `actor_value` int(11) DEFAULT NULL,
                     `use_notification` BOOLEAN NOT NULL DEFAULT TRUE,
@@ -40,6 +49,15 @@ class PluginFormcreatorTargetTicket_Actor extends CommonDBTM
             $query = "ALTER TABLE `$table`
             CHANGE COLUMN `actor_type` `actor_type`
             ENUM($enum_actor_type)
+            NOT NULL";
+            $DB->query($query) or die($DB->error());
+         }
+
+         $current_enum_role = PluginFormcreatorCommon::getEnumValues($table, 'actor_type');
+         if (count($current_enum_role) != count(self::getEnumRole())) {
+            $query = "ALTER TABLE `$table`
+            CHANGE COLUMN `actor_role` `actor_role`
+            ENUM($enum_actor_role)
             NOT NULL";
             $DB->query($query) or die($DB->error());
          }
