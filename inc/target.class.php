f28dd167b3535496d4af71d44972d7583d79ba70
btry <tbugier@teclib.com>
2016-12-20 11:44:27 +0100

    fix URL to edit image

--- inc/target.class.php
+++ inc/target.class.php
@@ -63,7 +63,7 @@ class PluginFormcreatorTarget extends CommonDBTM
          echo '</td>';
 
          echo '<td align="center" width="32">';
-         echo '<img src="'.$CFG_GLPI['root_doc'].'/plugins/formcreator/pics/pencil.png"
+         echo '<img src="'.$CFG_GLPI['root_doc'].'/plugins/formcreator/pics/edit.png"
                   alt="*" title="'.__('Edit');
          echo 'onclick="document.location=\'' . $targetItemUrl . '\'" align="absmiddle" style="cursor: pointer" /> ';
          echo '</td>';
