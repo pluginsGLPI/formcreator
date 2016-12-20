2b43142e0072a2163c76e334e3b5863bbe2a5d28
btry <tbugier@teclib.com>
2016-12-20 18:05:32 +0100

    fix question_conditions import

--- inc/question_condition.class.php
+++ inc/question_condition.class.php
@@ -12,23 +12,37 @@ class PluginFormcreatorQuestion_Condition extends CommonDBChild
     * @param  array   $condition the condition data (match the condition table)
     * @return integer the condition's id
     */
-   public static function import($questions_id = 0, $condition = array()) {
-      $item = new self;
+   public static function import($questions_id = 0, $condition = array(), $storeOnly = true) {
+      static $conditionsToImport = array();
 
-      $condition['plugin_formcreator_questions_id'] = $questions_id;
+      if ($storeOnly) {
+         $condition['plugin_formcreator_questions_id'] = $questions_id;
 
-      if ($conditions_id = plugin_formcreator_getFromDBByField($item, 'uuid', $condition['uuid'])) {
-         // add id key
-         $condition['id'] = $conditions_id;
+         $item = new static();
+         if ($conditions_id = plugin_formcreator_getFromDBByField($item, 'uuid', $condition['uuid'])) {
+            // add id key
+            $condition['id'] = $conditions_id;
 
-         // update condition
-         $item->update($condition);
+            // prepare update condition
+            $conditionsToImport[] = $condition;
+         } else {
+            // prepare create condition
+            $conditionsToImport[] = $condition;
+         }
       } else {
-         //create condition
-         $conditions_id = $item->add($condition);
+         // Assumes all questions needed for the stored conditions exist
+         foreach ($conditionsToImport as $condition) {
+            $item = new static();
+            $question = new PluginFormcreatorQuestion();
+            $condition['show_field'] = plugin_formcreator_getFromDBByField($question, 'uuid', $condition['show_field']);
+            if (isset($condition['id'])) {
+               $item->update($condition);
+            } else {
+               $item->add($condition);
+            }
+         }
+         $conditionsToImport = array();
       }
-
-      return $conditions_id;
    }
 
    /**
@@ -40,7 +54,10 @@ class PluginFormcreatorQuestion_Condition extends CommonDBChild
          return false;
       }
 
+      $question = new PluginFormcreatorQuestion();
+      $question->getFromDB($this->fields['show_field']);
       $condition = $this->fields;
+      $condition['show_field'] = $question->getField('uuid');
 
       unset($condition['id'],
             $condition['plugin_formcreator_questions_id']);
