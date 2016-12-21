06fda34ecbc8b5bb4afe85e0303eaabfd94566b0
btry <tbugier@teclib.com>
2016-12-21 09:03:37 +0100

    Revert "fix question_conditions import"
    This reverts commit 6ad7148b2132f139400d5e8aca5bbf01053c3702.

--- inc/form.class.php
+++ inc/form.class.php
@@ -1749,8 +1749,6 @@ class PluginFormcreatorForm extends CommonDBTM
             PluginFormcreatorSection::import($forms_id, $section);
          }
       }
-      // Save all question conditions stored in memory
-      PluginFormcreatorQuestion_Condition::import(0, array(), false);
 
       // import form's validators
       if ($forms_id
