0d80f7154685efc3bbd4302578314bf1e89810e9
btry <tbugier@teclib.com>
2017-01-11 17:43:46 +0100

    WIP : Hotfix/import export question condition (#388)
    * fix question_conditions import
    
    * fix question order during import

--- inc/form.class.php
+++ inc/form.class.php
@@ -1763,6 +1763,8 @@ class PluginFormcreatorForm extends CommonDBTM
             PluginFormcreatorSection::import($forms_id, $section);
          }
       }
+      // Save all question conditions stored in memory
+      PluginFormcreatorQuestion_Condition::import(0, array(), false);
 
       // import form's validators
       if ($forms_id
