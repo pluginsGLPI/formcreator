63e008629a2ad44a78aac550342b63a14c7afeb6
btry <tbugier@teclib.com>
2017-02-23 09:37:04 +0100

    fix php warning

--- setup.php
+++ setup.php
@@ -279,6 +279,6 @@ function plugin_formcrerator_upgrade_error(Migration $migration) {
    global $DB;
 
    $error = $DB->error();
-   $migration->log($error . "\n" . Toolbox::backtrace(false, '', array('Toolbox::backtrace()')));
+   $migration->log($error . "\n" . Toolbox::backtrace(false, '', array('Toolbox::backtrace()')), false);
    die($error . "<br><br> Please, check migration log");
 }
