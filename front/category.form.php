625be429adab70aff823e3d351fc2ac82fd0cbfe
btry <tbugier@teclib.com>
2017-05-12 09:28:07 +0200

    category is now a TreeDropdown

--- front/category.form.php
+++ front/category.form.php
@@ -7,12 +7,4 @@ Plugin::load('formcreator', true);
 
 $dropdown = new PluginFormcreatorCategory();
 
-if (isset($_POST['add']) && isset($_POST['name'])) {
-   $found = $dropdown->find("name LIKE '" . $_POST['name'] . "'");
-   if (!empty($found)) {
-      Session::addMessageAfterRedirect(__('A category already exists with the same name! Category creation failed.', 'formcreator'), false, ERROR);
-      Html::back();
-   }
-}
-
 include (GLPI_ROOT . "/front/dropdown.common.form.php");
