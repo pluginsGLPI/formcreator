e670ebd70532748ad0a3b90303beb80d3eab79b2
btry <tbugier@teclib.com>
2017-07-24 23:28:30 +0200

    fix wrong sql (thanks dcoronel1985)

--- inc/form.class.php
+++ inc/form.class.php
@@ -783,8 +783,8 @@ class PluginFormcreatorForm extends CommonDBTM
                 FROM glpi_plugin_formcreator_forms f
                 INNER JOIN glpi_plugin_formcreator_forms_validators fv ON fv.`plugin_formcreator_forms_id`=f.`id`
                 INNER JOIN glpi_plugin_formcreator_forms_answers fa ON f.`id` = fa.`plugin_formcreator_forms_id`
-                WHERE (f.`validation_required` = 1 AND fv.`items_id` = '$userId' AND fv.`itemtype` = 'User'
-                   OR f.`validation_required` = 2 AND fv.`items_id` IN ($groupIdList) AND fv.`itemtype` = 'Group'
+                WHERE (f.`validation_required` = 1 AND fv.`items_id` = '$userId' AND fv.`itemtype` = 'User' AND `fa`.`validator_id` = '$userId'
+                   OR f.`validation_required` = 2 AND fv.`items_id` IN ($groupIdList) AND fv.`itemtype` = 'Group' AND `fa`.`validator_id` IN ($groupIdList)
                 )
                 AND f.is_deleted = 0
                 ORDER BY fa.`status` ASC, fa.`request_date` DESC
