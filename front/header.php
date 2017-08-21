7d6e550b5b4f61dfd10ab7e4b2524ec58795e59b
btry <tbugier@teclib.com>
2017-08-21 14:51:06 +0200

    drop remaining files after removal of header feature

--- front/header.php
+++
@@ -1,7 +0,0 @@
-<?php
-include ('../../../inc/includes.php');
-
-Plugin::load('formcreator', true);
-
-$dropdown = new PluginFormcreatorHeader();
-include (GLPI_ROOT . "/front/dropdown.common.php");
