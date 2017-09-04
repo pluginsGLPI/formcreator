4569bef099a8c17bc14f68867b5692f65ed9daba
btry <tbugier@teclib.com>
2017-09-04 17:37:12 +0200

    fix upgrade

--- install/update_dev.php
+++ install/update_dev.php
@@ -19,32 +19,6 @@ function plugin_formcreator_updateForm_Answer_2_6(Migration $migration) {
    $migration->displayMessage("Upgrade glpi_plugin_formcreator_forms_answers");
 
    $table = 'glpi_plugin_formcreator_forms_answers';
-   $migration->changeField($table, 'validator_id', 'users_id_validator', 'integer');
-
-   // Rename key
-   $migration->dropKey($table, 'validator_id');
-   $migration->addKey($table, 'users_id_validator');
-
-   $migration->addField($table, 'groups_id_validator', 'integer', ['after' => 'requester_id']);
-   $migration->addKey($table, 'groups_id_validator');
-
-   // update questions
-   $question = new PluginFormcreatorQuestion();
-   $rows = $question->find("`fieldtype` = 'dropdown' AND `values` = 'ITILCategory'");
-   $table = PluginFormcreatorQuestion::getTable();
-   foreach ($rows as $id => $row) {
-      $updatedValue = json_encode([
-         'itemtype'                       => $row['values'],
-         'show_ticket_categories'         => 'both',
-         'show_ticket_categories_depth'   => 0
-      ]);
-      // Don't use update() method because the json will be HTML-entities-ified (see prepareInputForUpdate() )
-      $query = "UPDATE `$table` SET `values`='$updatedValue' WHERE `id`='$id'";
-      $DB->query($query) or plugin_formcreator_upgrade_error($migration);;
-   }
-
-   // Update Form Answers
-   $table = PluginFormcreatorForm_Answer::getTable();
    $migration->displayMessage("Upgrade $table");
 
    $migration->addField($table, 'users_id_validator', 'integer', ['after' => 'requester_id']);
@@ -53,7 +27,7 @@ function plugin_formcreator_updateForm_Answer_2_6(Migration $migration) {
    $migration->addKey($table, 'groups_id_validator');
    $migration->migrationOneTable($table);
 
-   $formTable = PluginFormcreatorForm::getTable();
+   $formTable = 'glpi_plugin_formcreator_forms';
    $query = "UPDATE `$table`
                 INNER JOIN `$formTable` ON (`$table`.`plugin_formcreator_forms_id` = `$formTable`.`id`)
                 SET `users_id_validator` = 'validator_id'
@@ -65,8 +39,24 @@ function plugin_formcreator_updateForm_Answer_2_6(Migration $migration) {
                 WHERE `$formTable`.`validation_required` = '2'";
    $DB->query($query) or plugin_formcreator_upgrade_error($migration);
 
+   $migration->dropKey($table, 'validator_id');
    $migration->dropField($table, 'validator_id');
 
+   // update questions
+   $table = 'glpi_plugin_formcreator_questions';
+   $migration->displayMessage("Upgrade $table");
+
+   $question = new PluginFormcreatorQuestion();
+   $rows = $question->find("`fieldtype` = 'dropdown' AND `values` = 'ITILCategory'");
+   foreach ($rows as $id => $row) {
+      $updatedValue = json_encode([
+         'itemtype'                       => $row['values'],
+         'show_ticket_categories'         => 'both',
+         'show_ticket_categories_depth'   => 0
+      ]);
+      // Don't use update() method because the json will be HTML-entities-ified (see prepareInputForUpdate() )
+      $query = "UPDATE `$table` SET `values`='$updatedValue' WHERE `id`='$id'";
+      $DB->query($query) or plugin_formcreator_upgrade_error($migration);;
    // add location rule
    $enum_location_rule = "'".implode("', '", array_keys(PluginFormcreatorTargetTicket::getEnumLocationRule()))."'";
    if (!FieldExists('glpi_plugin_formcreator_targettickets', 'location_rule', false)) {
@@ -89,4 +79,6 @@ function plugin_formcreator_updateForm_Answer_2_6(Migration $migration) {
       }
    }
    $migration->addField('glpi_plugin_formcreator_targettickets', 'location_question', 'integer', array('after' => 'location_rule'));
+
+   $migration->executeMigration();
 }
