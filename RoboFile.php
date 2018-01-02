7f555f7370edfd37b7224dd67e963dbfb2b857f5
btry <tbugier@teclib.com>
2018-01-02 15:37:22 +0100

    chore(build): update changelog

--- RoboFile.php
+++ RoboFile.php
@@ -1,4 +1,6 @@
 <?php
+use Robo\Task\Docker\Commit;
+
 /**
  * This is project's console commands configuration for Robo task runner.
  *
@@ -77,12 +79,12 @@ class RoboFile extends RoboFilePlugin
          throw new Exception("$version is not semver compliant. See http://semver.org/");
       }
 
-      if (!$this->tagExists($version)) {
-         throw new Exception("The tag $version does not exists yet");
+      if ($this->tagExists($version)) {
+         throw new Exception("The tag $version already exists");
       }
 
       if (!$this->isTagMatchesCurrentCommit($version)) {
-         throw new Exception("HEAD is not pointing to the tag of the version to build");
+         //throw new Exception("HEAD is not pointing to the tag of the version to build");
       }
 
       $versionTag = $this->getVersionTagFromXML($version);
@@ -90,10 +92,19 @@ class RoboFile extends RoboFilePlugin
          throw new Exception("The version does not exists in the XML file");
       }
 
+      // update version in package.json
+      $this->sourceUpdatePackageJson($version);
+      $this->sourceUpdateComposerJson($version);
+
+      $this->updateChangelog();
+
+      $this->gitCommit(['package.json', 'composer.json'], "docs: bump version in JSON files");
+      $this->gitCommit(['CHANGELOG.md'], "docs(changelog): update changelog");
+
       $pluginName = $this->getPluginName();
       $pluginPath = $this->getProjectPath();
       $targetFile = $pluginPath. "/dist/glpi-" . $this->getPluginName() . "-$version.tar.bz2";
-      $toArchive = implode(' ', $this->getFileToArchive($version));
+      $toArchive = implode(' ', $this->getFileToArchive("HEAD"));
       @mkdir($pluginPath. "/dist");
       $this->_exec("git archive --prefix=$pluginName/ $version $toArchive | bzip2 > $targetFile");
    }
@@ -208,6 +219,40 @@ class RoboFile extends RoboFilePlugin
       return false;
    }
 
+   /**
+    * @param array $files files to commit
+    * @param string $commitMessage commit message
+    */
+   protected function gitCommit(array $files, $commitMessage) {
+       if (count($files) < 1) {
+           $arg = '-u';
+       } else {
+           $arg = '"' . implode('" "', $files) . '"';
+       }
+       exec("git add $arg", $output, $retCode);
+       if ($retCode > 0) {
+           throw new Exception("Failed to add files for $commitMessage");
+       }
+
+       exec("git commit -m \"$commitMessage\"", $output, $retCode);
+       if ($retCode > 0) {
+           throw new Exception("Failed to commit $commitMessage");
+       }
+
+       return true;
+   }
+
+   /**
+    */
+   protected function updateChangelog() {
+       exec("node_modules/.bin/conventional-changelog -p angular -i CHANGELOG.md -s", $output, $retCode);
+       if ($retCode > 0) {
+           throw new Exception("Failed to update the changelog");
+       }
+
+       return true;
+   }
+
    public function localesExtract() {
       $potfile = strtolower("glpi.pot");
       $phpSources = "*.php ajax/*.php front/*.php inc/*.php inc/fields/*.php install/*.php js/*.php";
@@ -242,4 +287,37 @@ class RoboFile extends RoboFilePlugin
       return $this;
    }
 
+   /**
+    *
+    * @param string $filename
+    * @param string $version
+    */
+   protected function updateJsonFile($filename, $version) {
+       // get Package JSON
+       $filename = __DIR__ . "/$filename";
+       $jsonContent = file_get_contents($filename);
+       $jsonContent = json_decode($jsonContent, true);
+
+       // update version
+       if (empty($version)) {
+           echo "Version not found in setup.php\n";
+           return;
+       }
+       $jsonContent['version'] = $version;
+       file_put_contents($filename, json_encode($jsonContent, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . "\n");
+   }
+
+   /**
+    * @param $version
+    */
+   protected function sourceUpdatePackageJson($version) {
+       $this->updateJsonFile('package.json', $version);
+   }
+
+   /**
+    * @param string $version
+    */
+   protected function sourceUpdateComposerJson($version) {
+       $this->updateJsonFile('composer.json', $version);
+   }
 }
