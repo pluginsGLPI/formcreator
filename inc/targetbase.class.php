d27ac73cef6458b974404cc0da6db464d69dd07d
btry <tbugier@teclib.com>
2018-01-29 17:12:39 +0100

    fix(field): fix quote escaping for file field
    fix #832

--- inc/targetbase.class.php
+++ inc/targetbase.class.php
@@ -771,7 +771,7 @@ EOS;
                $content = str_replace('##answer_' . $id . '##', addslashes($value), $content);
             } else {
                if (strpos($content, '##answer_' . $id . '##') !== false) {
-                  $content = str_replace('##question_' . $id . '##', $name, $content);
+                  $content = str_replace('##question_' . $id . '##', addslashes($name), $content);
                   if ($value !== '') {
                      $content = str_replace('##answer_' . $id . '##', __('Attached document', 'formcreator'), $content);
 
