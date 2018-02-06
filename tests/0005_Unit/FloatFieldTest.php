33e66b6f590427405237f8069a8d08609148e2d7
btry <tbugier@teclib.com>
2018-02-06 15:44:37 +0100

    refactor: short array and code style

--- tests/0005_Unit/FloatFieldTest.php
+++ tests/0005_Unit/FloatFieldTest.php
@@ -2,88 +2,88 @@
 class FloatFieldTest extends SuperAdminTestCase {
 
    public function provider() {
-      $dataset = array(
-            array(
-                  'fields'          => array(
-                        'fieldtype'       => 'float',
-                        'name'            => 'question',
-                        'required'        => '0',
-                        'default_values'  => '',
-                        'order'           => '1',
-                        'show_rule'       => 'always',
-                        'show_empty'      => '0',
-                        'values'          => ''
-                  ),
-                  'data'            => null,
-                  'expectedValue'   => '',
-                  'expectedIsValid' => true
-            ),
-            array(
-                  'fields'          => array(
-                        'fieldtype'       => 'float',
-                        'name'            => 'question',
-                        'required'        => '0',
-                        'default_values'  => '2',
-                        'order'           => '1',
-                        'show_rule'       => 'always',
-                        'show_empty'      => '0',
-                        'values'          => ''
-                  ),
-                  'data'            => null,
-                  'expectedValue'   => '2',
-                  'expectedIsValid' => true
-            ),
-            array(
-                  'fields'          => array(
-                        'fieldtype'       => 'float',
-                        'name'            => 'question',
-                        'required'        => '0',
-                        'default_values'  => "2",
-                        'order'           => '1',
-                        'show_rule'       => 'always',
-                        'show_empty'      => '0',
-                        'range_min'       => 3,
-                        'range_max'       => 4,
-                  ),
-                  'data'            => null,
-                  'expectedValue'   => '2',
-                  'expectedIsValid' => false
-            ),
-            array(
-                  'fields'          => array(
-                        'fieldtype'       => 'float',
-                        'name'            => 'question',
-                        'required'        => '0',
-                        'default_values'  => "5",
-                        'order'           => '1',
-                        'show_rule'       => 'always',
-                        'show_empty'      => '0',
-                        'values'          => '',
-                        'range_min'       => 3,
-                        'range_max'       => 4,
-                  ),
-                  'data'            => null,
-                  'expectedValue'   => '5',
-                  'expectedIsValid' => false
-            ),
-            array(
-                  'fields'          => array(
-                        'fieldtype'       => 'float',
-                        'name'            => 'question',
-                        'required'        => '0',
-                        'default_values'  => "3.141592",
-                        'order'           => '1',
-                        'show_rule'       => 'always',
-                        'show_empty'      => '0',
-                        'values'          => '',
-                        'range_min'       => 3,
-                        'range_max'       => 4,
-                  ),
-                  'data'            => null,
-                  'expectedValue'   => '3.141592',
-                  'expectedIsValid' => true
-            ),
-      );
+      $dataset = [
+         [
+            'fields'          => [
+               'fieldtype'       => 'float',
+               'name'            => 'question',
+               'required'        => '0',
+               'default_values'  => '',
+               'order'           => '1',
+               'show_rule'       => 'always',
+               'show_empty'      => '0',
+               'values'          => ''
+            ],
+            'data'            => null,
+            'expectedValue'   => '',
+            'expectedIsValid' => true
+         ],
+         [
+            'fields'          => [
+               'fieldtype'       => 'float',
+               'name'            => 'question',
+               'required'        => '0',
+               'default_values'  => '2',
+               'order'           => '1',
+               'show_rule'       => 'always',
+               'show_empty'      => '0',
+               'values'          => ''
+            ],
+            'data'            => null,
+            'expectedValue'   => '2',
+            'expectedIsValid' => true
+         ],
+         [
+            'fields'          => [
+               'fieldtype'       => 'float',
+               'name'            => 'question',
+               'required'        => '0',
+               'default_values'  => "2",
+               'order'           => '1',
+               'show_rule'       => 'always',
+               'show_empty'      => '0',
+               'range_min'       => 3,
+               'range_max'       => 4,
+            ],
+            'data'            => null,
+            'expectedValue'   => '2',
+            'expectedIsValid' => false
+         ],
+         [
+            'fields'          => [
+               'fieldtype'       => 'float',
+               'name'            => 'question',
+               'required'        => '0',
+               'default_values'  => "5",
+               'order'           => '1',
+               'show_rule'       => 'always',
+               'show_empty'      => '0',
+               'values'          => '',
+               'range_min'       => 3,
+               'range_max'       => 4,
+            ],
+            'data'            => null,
+            'expectedValue'   => '5',
+            'expectedIsValid' => false
+         ],
+         [
+            'fields'          => [
+               'fieldtype'       => 'float',
+               'name'            => 'question',
+               'required'        => '0',
+               'default_values'  => "3.141592",
+               'order'           => '1',
+               'show_rule'       => 'always',
+               'show_empty'      => '0',
+               'values'          => '',
+               'range_min'       => 3,
+               'range_max'       => 4,
+            ],
+            'data'            => null,
+            'expectedValue'   => '3.141592',
+            'expectedIsValid' => true
+         ],
+      ];
 
       return $dataset;
    }
