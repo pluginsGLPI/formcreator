3acad96c85b177033e703c6224b52fc1883d2ba4
btry <tbugier@teclib.com>
2018-04-30 10:52:45 +0200

    feat(installer): update installer for release"
    Signed-off-by: btry <tbugier@teclib.com>

--- install/update_2.6.2_2.6.3.php
+++ install/update_2.6.2_2.6.3.php
@@ -36,8 +36,49 @@
 function plugin_formcreator_update_2_6_3() {
    global $DB;
 
+   // Change id of search option for status of form_answer
+   $table = 'glpi_displaypreferences';
+   $query = "UPDATE `$table` SET `num`='8' WHERE `itemtype`='PluginFormcreatorForm_Answer' AND `num`='1'";
+   $DB->query($query);
+
    // Remove abusive encding in sections
    $table = 'glpi_plugin_formcreator_sections';
+   $request = [
+      'FROM' => $table,
+   ];
+   foreach ($DB->request($request) as $row) {
+      $id = $row['id'];
+      $name = Toolbox::addslashes_deep(html_entity_decode($row['name'], ENT_QUOTES|ENT_HTML5));
+      $id = $row['id'];
+      $DB->query("UPDATE `$table` SET `name`='$name' WHERE `id` = '$id'");
+   }
+
+   // Remove abusive encoding in targets
+   $table = 'glpi_plugin_formcreator_targets';
+   $request = [
+      'FROM' => $table,
+   ];
+   foreach ($DB->request($request) as $row) {
+      $id = $row['id'];
+      $name = Toolbox::addslashes_deep(html_entity_decode($row['name'], ENT_QUOTES|ENT_HTML5));
+      $id = $row['id'];
+      $DB->query("UPDATE `$table` SET `name`='$name' WHERE `id` = '$id'");
+   }
+
+   // Remove abusive encding in target tickets
+   $table = 'glpi_plugin_formcreator_targettickets';
+   $request = [
+      'FROM' => $table,
+   ];
+   foreach ($DB->request($request) as $row) {
+      $id = $row['id'];
+      $name = Toolbox::addslashes_deep(html_entity_decode($row['name'], ENT_QUOTES|ENT_HTML5));
+      $id = $row['id'];
+      $DB->query("UPDATE `$table` SET `name`='$name' WHERE `id` = '$id'");
+   }
+
+   // Remove abusive encding in target changes
+   $table = 'glpi_plugin_formcreator_targetchanges';
    $request = [
       'FROM' => $table,
    ];
