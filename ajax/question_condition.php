63464a201d3eabae1ee2408b7f2070e948819f35
Thierry Bugier <tbugier@teclib.com>
2018-08-01 17:01:53 +0200

    fix(question_condition): fix creation of conditions
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- ajax/question_condition.php
+++ ajax/question_condition.php
@@ -32,24 +32,16 @@
  */
 
 include ('../../../inc/includes.php');
-Session::checkRight("entity", UPDATE);
+Session::checkRight('entity', UPDATE);
 
 if (!isset($_REQUEST['plugin_formcreator_questions_id'])) {
    exit;
 }
-$questionId = (int) $_REQUEST['plugin_formcreator_questions_id'];
-if ($questionId == 0) {
-   $formId = (int) $_REQUEST['plugin_formcreator_forms_id'];
-} else {
-   $form = new PluginFormcreatorForm();
-   $form->getByQuestionId($questionId);
-   $formId = $form->getID();
-}
 
 if (isset($_REQUEST['_empty'])) {
    // get an empty condition HTML table row
-   $form = new PluginFormcreatorForm();
-   $form->getByQuestionId($questionId);
+   $formId = (int) $_REQUEST['plugin_formcreator_forms_id'];
+   $questionId = (int) $_REQUEST['plugin_formcreator_questions_id'];
    $questionCondition = new PluginFormcreatorQuestion_Condition();
    echo $questionCondition->getConditionHtml($formId, $questionId);
 }
