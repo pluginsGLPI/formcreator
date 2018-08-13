7c02062097d9c92cd583c1f53169f0dd6ee55f94
btry <tbugier@teclib.com>
2018-08-13 10:49:29 +0200

    chore: bump version
    Signed-off-by: btry <tbugier@teclib.com>

--- setup.php
+++ setup.php
@@ -33,11 +33,11 @@
 
 global $CFG_GLPI;
 // Version of the plugin
-define('PLUGIN_FORMCREATOR_VERSION', 'develop');
+define('PLUGIN_FORMCREATOR_VERSION', '2.6.4');
 // Schema version of this version
 define('PLUGIN_FORMCREATOR_SCHEMA_VERSION', '2.6');
 // is or is not an official release of the plugin
-define('PLUGIN_FORMCREATOR_IS_OFFICIAL_RELEASE', false);
+define('PLUGIN_FORMCREATOR_IS_OFFICIAL_RELEASE', true);
 
 
 // Minimal GLPI version, inclusive
