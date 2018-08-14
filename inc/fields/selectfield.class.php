8e7d184bce990b197392bc34ac7db5aba024d6cb
btry <tbugier@teclib.com>
2018-08-14 14:29:21 +0200

    ci(travis): drop support for GLPI 9.2.x
    Signed-off-by: btry <tbugier@teclib.com>

--- inc/fields/selectfield.class.php
+++ inc/fields/selectfield.class.php
@@ -43,11 +43,7 @@ class PluginFormcreatorSelectField extends PluginFormcreatorField
          if (!empty($this->fields['values'])) {
             foreach ($values as $value) {
                if ((trim($value) != '')) {
-                  if (version_compare(GLPI_VERSION, '9.2.1') <= 0) {
-                     $tab_values[Html::entities_deep($value)] = $value;
-                  } else {
-                     $tab_values[$value] = $value;
-                  }
+                  $tab_values[$value] = $value;
                }
             }
 
