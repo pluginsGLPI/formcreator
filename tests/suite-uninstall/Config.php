ed25ca1de1372262a116eff1a469cf4664868fcf
btry <tbugier@teclib.com>
2018-08-29 17:45:28 +0200

    docs: update/add headers
    Signed-off-by: btry <tbugier@teclib.com>

--- tests/suite-uninstall/Config.php
+++ tests/suite-uninstall/Config.php
@@ -1,32 +1,33 @@
 <?php
 /**
+ * ---------------------------------------------------------------------
+ * Formcreator is a plugin which allows creation of custom forms of
+ * easy access.
+ * ---------------------------------------------------------------------
  * LICENSE
  *
- * Copyright © 2016-2018 Teclib'
- * Copyright © 2010-2018 by the FusionInventory Development Team.
+ * This file is part of Formcreator.
  *
- * This file is part of Flyve MDM Plugin for GLPI.
- *
- * Flyve MDM Plugin for GLPI is a subproject of Flyve MDM. Flyve MDM is a mobile
- * device management software.
- *
- * Flyve MDM Plugin for GLPI is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
+ * Formcreator is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * Flyve MDM Plugin for GLPI is distributed in the hope that it will be useful,
+ *
+ * Formcreator is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- * You should have received a copy of the GNU Affero General Public License
- * along with Flyve MDM Plugin for GLPI. If not, see http://www.gnu.org/licenses/.
- * ------------------------------------------------------------------------------
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Formcreator. If not, see <http://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------
  * @author    Thierry Bugier
- * @copyright Copyright © 2018 Teclib
- * @license   AGPLv3+ http://www.gnu.org/licenses/agpl.txt
- * @link      https://github.com/flyve-mdm/glpi-plugin
- * @link      https://flyve-mdm.com/
- * ------------------------------------------------------------------------------
+ * @copyright Copyright © 2011 - 2018 Teclib'
+ * @license   GPLv3+ http://www.gnu.org/licenses/gpl.txt
+ * @link      https://github.com/pluginsGLPI/formcreator/
+ * @link      https://pluginsglpi.github.io/formcreator/
+ * @link      http://plugins.glpi-project.org/#/plugin/formcreator
+ * ---------------------------------------------------------------------
  */
 
 namespace tests\units;
