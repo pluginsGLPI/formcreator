5d58a88323031cdad9b33b303f3f1d5341bfa10c
Thierry Bugier <tbugier@teclib.com>
2018-09-15 16:05:36 +0200

    refactor: code refactor
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- ajax/dropdown_values.php
+++ ajax/dropdown_values.php
@@ -33,7 +33,7 @@
 
 include ('../../../inc/includes.php');
 
-Session::checkRight("entity", UPDATE);
+Session::checkRight('entity', UPDATE);
 
 if ($_REQUEST['dropdown_itemtype'] == '0' || !class_exists($_REQUEST['dropdown_itemtype'])) {
    Dropdown::showFromArray("dropdown_default_value", [], ['display_emptychoice'   => true]);
