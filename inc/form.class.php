a3d9d0d966cbc45e48d256ded0f8c31db0f8550a
btry <tbugier@teclib.com>
2018-09-27 14:15:14 +0200

    fix(form): duplicate with quote
    if a single quote appears in fields of a section or a question the duplication fails and the duplicated form is incomplete
    
    Signed-off-by: btry <tbugier@teclib.com>

--- inc/form.class.php
+++ inc/form.class.php
@@ -1330,6 +1330,7 @@ class PluginFormcreatorForm extends CommonDBTM implements PluginFormcreatorExpor
          unset($sectionRow['id'],
                $sectionRow['uuid']);
          $sectionRow['plugin_formcreator_forms_id'] = $new_form_id;
+         $sectionRow = Toolbox::addslashes_deep($sectionRow);
          if (!$new_sections_id = $form_section->add($sectionRow)) {
             return false;
          }
@@ -1340,6 +1341,7 @@ class PluginFormcreatorForm extends CommonDBTM implements PluginFormcreatorExpor
             unset($questionRow['id'],
                   $questionRow['uuid']);
             $questionRow['plugin_formcreator_sections_id'] = $new_sections_id;
+            $questionRow = Toolbox::addslashes_deep($questionRow);
             $questionRow['_skip_checks'] = true;
             if (!$new_questions_id = $section_question->add($questionRow)) {
                return false;
