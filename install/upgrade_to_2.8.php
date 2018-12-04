8ec585056243c7aad134daf3e64dd6db7e5cc5b4
Fred Mohier <frederic.mohier@gmail.com>
2018-12-04 13:27:50 +0100

    Update function for 2.6 to 2.7 (fix missing parameter)
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- install/upgrade_to_2.8.php
+++ install/upgrade_to_2.8.php
@@ -50,5 +50,17 @@ class PluginFormcreatorUpgradeTo2_8 {
          'actor_type',
          "ENUM('creator','validator','person','question_person','group','question_group','supplier','question_supplier','question_actors') NOT NULL"
       );
+
+      // Upgrade plugin configuration table
+      $table = 'glpi_plugin_formcreator_entityconfigs';
+      $migration->displayMessage("Upgrade $table");
+      $migration->addField($table, 'external_links_prefix', 'string', ['after' => 'replace_helpdesk']);
+      $migration->addField($table, 'external_links_icon', 'string', ['after' => 'external_links_prefix']);
+      $migration->addField($table, 'external_links_title', 'string', ['after' => 'external_links_icon']);
+      $migration->addField($table, 'tickets_summary', 'integer', ['after' => 'external_links_title', 'value' => '1']);
+      $migration->addField($table, 'user_preferences', 'integer', ['after' => 'tickets_summary', 'value' => '1']);
+      $migration->addField($table, 'avatar', 'integer', ['after' => 'user_preferences', 'value' => '1']);
+      $migration->addField($table, 'user_name', 'integer', ['after' => 'avatar', 'value' => '0']);
+      $migration->addField($table, 'profile_selector', 'integer', ['after' => 'user_name', 'value' => '1']);
    }
 }
