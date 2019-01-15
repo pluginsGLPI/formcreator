db8d4d25022dd96d3f0a429234cd9490f1ff5ed2
Thierry Bugier <tbugier@teclib.com>
2019-01-15 18:24:53 +0100

    build: bump version
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- setup.php
+++ setup.php
@@ -33,11 +33,11 @@
 
 global $CFG_GLPI;
 // Version of the plugin
-define('PLUGIN_FORMCREATOR_VERSION', '2.7.0-beta.1');
+define('PLUGIN_FORMCREATOR_VERSION', '2.7.0-beta.2');
 // Schema version of this version
 define('PLUGIN_FORMCREATOR_SCHEMA_VERSION', '2.7');
 // is or is not an official release of the plugin
-define('PLUGIN_FORMCREATOR_IS_OFFICIAL_RELEASE', false);
+define('PLUGIN_FORMCREATOR_IS_OFFICIAL_RELEASE', true);
 
 // Minimal GLPI version, inclusive
 define ('PLUGIN_FORMCREATOR_GLPI_MIN_VERSION', '9.3.0');
