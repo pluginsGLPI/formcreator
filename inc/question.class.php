cba0be3104f4f6a3b1d60bef77fe0a3fb67833e7
Thierry Bugier <tbugier@teclib.com>
2019-02-11 18:07:52 +0100

    refactor(targetticket,targetchange): factorize actors edition
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/question.class.php
+++ inc/question.class.php
@@ -1663,6 +1663,6 @@ class PluginFormcreatorQuestion extends CommonDBChild implements PluginFormcreat
          $items[$question['sname']][$question['qid']] = $question['qname'];
       }
 
-      Dropdown::showFromArray($name, $items, $opt);
+      Dropdown::showFromArray($name, $items, []);
    }
 }
