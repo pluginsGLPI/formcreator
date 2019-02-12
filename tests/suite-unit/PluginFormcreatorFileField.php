4d96977ee16a1db11d23cd2c59661aca368df214
Thierry Bugier <tbugier@teclib.com>
2019-02-12 09:18:00 +0100

    docs: update headers
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- tests/suite-unit/PluginFormcreatorFileField.php
+++ tests/suite-unit/PluginFormcreatorFileField.php
@@ -21,7 +21,8 @@
  * You should have received a copy of the GNU General Public License
  * along with Formcreator. If not, see <http://www.gnu.org/licenses/>.
  * ---------------------------------------------------------------------
- * @copyright Copyright © 2011 - 2018 Teclib'
+ *
+ * @copyright Copyright © 2011 - 2019 Teclib'
  * @license   http://www.gnu.org/licenses/gpl.txt GPLv3+
  * @link      https://github.com/pluginsGLPI/formcreator/
  * @link      https://pluginsglpi.github.io/formcreator/
