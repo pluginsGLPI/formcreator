fcb357be011c14028c6f4ae89d83f2d68459c97b
Thierry Bugier <tbugier@teclib.com>
2019-02-27 19:22:34 +0100

    fix(targetchange): add users from question of type actors
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- setup.php
+++ setup.php
@@ -33,11 +33,11 @@
 
 global $CFG_GLPI;
 // Version of the plugin
-define('PLUGIN_FORMCREATOR_VERSION', '2.7.0');
+define('PLUGIN_FORMCREATOR_VERSION', '2.8.0');
 // Schema version of this version
-define('PLUGIN_FORMCREATOR_SCHEMA_VERSION', '2.7');
+define('PLUGIN_FORMCREATOR_SCHEMA_VERSION', '2.8');
 // is or is not an official release of the plugin
-define('PLUGIN_FORMCREATOR_IS_OFFICIAL_RELEASE', true);
+define('PLUGIN_FORMCREATOR_IS_OFFICIAL_RELEASE', false);
 
 // Minimal GLPI version, inclusive
 define ('PLUGIN_FORMCREATOR_GLPI_MIN_VERSION', '9.3.0');
