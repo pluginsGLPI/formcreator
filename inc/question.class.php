dea1a9f62923e94ae6cccc2959c82a90edf518ab
Thierry Bugier <tbugier@teclib.com>
2019-02-28 18:32:01 +0100

    style: fix code style
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/question.class.php
+++ inc/question.class.php
@@ -1182,7 +1182,7 @@ class PluginFormcreatorQuestion extends CommonDBChild implements PluginFormcreat
       ]);
 
       $questions = [];
-      foreach($result as $row) {
+      foreach ($result as $row) {
          $question = new self();
          $question->getFromDB($row['id']);
          $questions[$row['id']] = $question;
