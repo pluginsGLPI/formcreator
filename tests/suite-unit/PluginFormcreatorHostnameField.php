9d49504b57df9dc5406f80fdac6c9c57b4d4e441
Thierry Bugier <tbugier@teclib.com>
2019-04-15 10:30:00 +0200

    test: add tests
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- tests/suite-unit/PluginFormcreatorHostnameField.php
+++ tests/suite-unit/PluginFormcreatorHostnameField.php
@@ -46,4 +46,228 @@ class PluginFormcreatorHostnameField extends CommonTestCase {
       $output = $instance->canRequire();
       $this->boolean($output)->isFalse();
    }
+
+   public function getDesignSpecializationField() {
+      $instance = new \PluginFormcreatorHostnameField([]);
+      $output = $instance->getDesignSpecializationField();
+      $this->array($output)->isIdenticalTo([
+         'label' => '',
+         'field' => '',
+         'additions' => '',
+         'may_be_empty' => false,
+         'may_be_required' => false,
+      ]);
+   }
+
+   public function providerSerializeValue() {
+      return [
+         [
+            'value' => '',
+            'expected' => '',
+         ],
+         [
+            'value' => "foo",
+            'expected' => "foo",
+         ],
+      ];
+   }
+
+   /**
+    * @dataProvider providerSerializeValue
+    */
+   public function serializeValue($value, $expected) {
+      $instance = new \PluginFormcreatorHostnameField([]);
+      $instance->prepareQuestionInputForSave([
+         'default_values' => $value,
+      ]);
+      $output = $instance->serializeValue();
+      $this->string($output)->isEqualTo($expected);
+   }
+
+   public function providerDeserializeValue() {
+      return [
+         [
+            'value'     => '',
+            'expected'  => '',
+         ],
+         [
+            'value'     => 'foo',
+            'expected'  => 'foo' ,
+         ],
+      ];
+   }
+
+   /**
+    * @dataProvider providerDeserializeValue
+    */
+   public function testDeserializeValue($value, $expected) {
+      $instance = new \PluginFormcreatorHostnameField([]);
+      $instance->deserializeValue($value);
+      $output = $instance->getValueForTargetText(false);
+      $this->string($output)->isEqualTo($expected);
+   }
+
+   public function testGetValueForDesign() {
+      $value = 'foo';
+      $instance = new \PluginFormcreatorHostnameField([]);
+      $instance->deserializeValue($value);
+      $output = $instance->getValueForDesign();
+      $this->string($output)->isEqualTo('');
+   }
+   
+   public function providerGetValueForTargetText() {
+      return [
+         [
+            'fields' => [
+               'values' => ''
+            ],
+            'value' => "",
+            'expected' => ''
+         ],
+         [
+            'fields' => [
+               'values' => ""
+            ],
+            'value' => "foo",
+            'expected' => 'foo'
+         ],
+      ];
+   }
+
+   /**
+    * @dataprovider providerGetValueForTargetText
+    */
+   public function testGetValueForTargetText($fields, $value, $expected) {
+      $instance = $this->newTestedInstance($fields);
+      $instance->deserializeValue($value);
+
+      $output = $instance->getValueForTargetText(true);
+      $this->string($output)->isEqualTo($expected);
+   }
+
+   public function providerEquals() {
+      return [
+         [
+            'value'     => '',
+            'answer'    => '',
+            'expected'  => true,
+         ],
+         [
+            'value'     => 'foo',
+            'answer'    => 'bar',
+            'expected'  => false,
+         ],
+         [
+            'value'     => 'foo',
+            'answer'    => 'foo',
+            'expected'  => true,
+         ],
+      ];
+   }
+
+   /**
+    * @dataProvider providerEquals
+    */
+   public function testEquals($value, $answer, $expected) {
+      $instance = new \PluginFormcreatorHostnameField(['id' => '1']);
+      $instance->parseAnswerValues(['formcreator_field_1' => $answer]);
+      $this->boolean($instance->equals($value))->isEqualTo($expected);
+   }
+
+   public function providerNotEquals() {
+      return [
+         [
+            'value'     => '',
+            'answer'    => '',
+            'expected'  => false,
+         ],
+         [
+            'value'     => 'foo',
+            'answer'    => 'bar',
+            'expected'  => true,
+         ],
+         [
+            'value'     => 'foo',
+            'answer'    => 'foo',
+            'expected'  => false,
+         ],
+      ];
+   }
+
+   /**
+    * @dataProvider providerNotEquals
+    */
+    public function testNotEquals($value, $answer, $expected) {
+      $instance = new \PluginFormcreatorHostnameField(['id' => '1'], $answer);
+      $instance->parseAnswerValues(['formcreator_field_1' => $answer]);
+      $this->boolean($instance->notEquals($value))->isEqualTo($expected);
+   }
+
+   public function providerGreaterThan() {
+      return [
+         [
+            'value'     => '',
+            'answer'    => '',
+            'expected'  => false,
+         ],
+         [
+            'value'     => 'foo',
+            'answer'    => 'bar',
+            'expected'  => false,
+         ],
+         [
+            'value'     => 'bar',
+            'answer'    => 'foo',
+            'expected'  => true,
+         ],
+         [
+            'value'     => 'foo',
+            'answer'    => 'foo',
+            'expected'  => false,
+         ],
+      ];
+   }
+
+   /**
+    * @dataProvider providerGreaterThan
+    */
+    public function testGreaterThan($value, $answer, $expected) {
+      $instance = new \PluginFormcreatorHostnameField(['id' => '1'], $answer);
+      $instance->parseAnswerValues(['formcreator_field_1' => $answer]);
+      $this->boolean($instance->greaterThan($value))->isEqualTo($expected);
+   }
+
+   public function providerLessThan() {
+      return [
+         [
+            'value'     => '',
+            'answer'    => '',
+            'expected'  => false,
+         ],
+         [
+            'value'     => 'foo',
+            'answer'    => 'bar',
+            'expected'  => true,
+         ],
+         [
+            'value'     => 'bar',
+            'answer'    => 'foo',
+            'expected'  => false,
+         ],
+         [
+            'value'     => 'foo',
+            'answer'    => 'foo',
+            'expected'  => false,
+         ],
+      ];
+   }
+
+   /**
+    * @dataProvider providerLessThan
+    */
+    public function testLessThan($value, $answer, $expected) {
+      $instance = new \PluginFormcreatorHostnameField(['id' => '1'], $answer);
+      $instance->parseAnswerValues(['formcreator_field_1' => $answer]);
+      $this->boolean($instance->lessThan($value))->isEqualTo($expected);
+   }
 }
