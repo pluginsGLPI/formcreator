571df563a10ad1cffdbe1e0f41f9ae0c7ecd68a6
Thierry Bugier <tbugier@teclib.com>
2019-05-10 11:58:38 +0200

    test: remove useless test
    implemented in suite-unit
    
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- tests/suite-integration/PluginFormcreatorQuestion.php
+++ tests/suite-integration/PluginFormcreatorQuestion.php
@@ -42,36 +42,4 @@ class PluginFormcreatorQuestion extends CommonTestCase {
             break;
       }
    }
-
-   public function testDuplicate() {
-      $section = $this->getSection();
-
-      $question = new \PluginFormcreatorQuestion();
-      $questions_id_1 = $question->add(['name'                           => "test clone question 1",
-                                        'fieldtype'                      => 'text',
-                                        'plugin_formcreator_sections_id' => $section->getID(),
-                                        'default_values' => '',
-                                        '_parameters' => [
-                                           'text' => [
-                                           'regex' => ['regex' => ''],
-                                           'range' => ['min' => '', 'max' => ''],
-                                           ]
-                                         ],
-                                        ]);
-
-      //clone the question
-      $this->integer($question->duplicate());
-
-      //get cloned section
-      $originalId = $question->getID();
-      $new_question  = new \PluginFormcreatorQuestion();
-      $new_question->getFromDBByCrit([
-          'AND' => [
-              'name'                           => 'test clone question 1',
-              'NOT'                            => ['uuid' => $question->getField('uuid')],  // operator <> available in GLPI 9.3+ only
-              'plugin_formcreator_sections_id' => $question->getField('plugin_formcreator_sections_id')
-          ]
-      ]);
-      $this->boolean($new_question->isNewItem())->isFalse();
-   }
 }
