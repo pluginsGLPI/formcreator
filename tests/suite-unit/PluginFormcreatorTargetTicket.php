4e9a178e29356cfa51ae61c6127132566efb361d
Thierry Bugier <tbugier@teclib.com>
2019-06-17 14:16:14 +0200

    test: try to debug
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- tests/suite-unit/PluginFormcreatorTargetTicket.php
+++ tests/suite-unit/PluginFormcreatorTargetTicket.php
@@ -637,6 +637,58 @@ class PluginFormcreatorTargetTicket extends CommonTestCase {
          ],
       ];
 
+      // $instance = new PluginFormcreatorTargetTicketDummy();
+      // $question = $this->getQuestion([
+      //    'fieldtype' => 'dropdown',
+      //    'values' => json_encode([
+      //       'itemtype' => 'ITILCategory',
+      //       'show_ticket_categories' =>'both',
+      //       'show_ticket_categories_depth' => '0',
+      //       'show_ticket_categories_root' => '0',
+      //    ]),
+      // ]);
+      // $form = new \PluginFormcreatorForm();
+      // $form->getByQuestionId($question->getID());
+      // $fields = $form->getFields();
+      // $instance->add([
+      //    'plugin_formcreator_forms_id' => $form->getID(),
+      //    'category_rule' => \PluginFormcreatorTargetTicket::CATEGORY_RULE_SPECIFIC,
+      //    'category_question' => '41',
+      // ]);
+      // $input = [
+      //    'formcreator_field_' . $question->getID() => '42',
+      // ];
+      // foreach ($fields as $id => $field) {
+      //    $field->parseAnswerValues($input);
+      // }
+      // $formAnswer = new \PluginFormcreatorFormAnswer();
+      // $this->disableDebug();
+      // $formAnswer->saveAnswers(
+      //    $form,
+      //    $input,
+      //    $fields
+      // );
+      // $this->restoreDebug();
+
+      // $dataSet[] = [
+      //    'instance' => $instance,
+      //    'data' => [
+      //    ],
+      //    'formanswer' => $formAnswer,
+      //    'expected' => [
+      //       'itilcategories_id' => '41',
+      //    ],
+      // ];
+
+      return $dataSet;
+   }
+
+   /**
+    *
+    */
+   public function testSetTargetCategory() {
+      $dataSet = [];
+
       $instance = new PluginFormcreatorTargetTicketDummy();
       $question = $this->getQuestion([
          'fieldtype' => 'dropdown',
@@ -652,8 +704,8 @@ class PluginFormcreatorTargetTicket extends CommonTestCase {
       $fields = $form->getFields();
       $instance->add([
          'plugin_formcreator_forms_id' => $form->getID(),
-         'category_rule' => \PluginFormcreatorTargetTicket::CATEGORY_RULE_SPECIFIC,
-         'category_question' => '41',
+         'category_rule' => \PluginFormcreatorTargetTicket::CATEGORY_RULE_ANSWER,
+         'category_question' => $question->getID(),
       ]);
       $input = [
          'formcreator_field_' . $question->getID() => '42',
@@ -668,26 +720,23 @@ class PluginFormcreatorTargetTicket extends CommonTestCase {
          $input,
          $fields
       );
-      $this->restoreDebug();
+      // $this->restoreDebug();
 
-      $dataSet[] = [
-         'instance' => $instance,
-         'data' => [
-         ],
-         'formanswer' => $formAnswer,
-         'expected' => [
-            'itilcategories_id' => '41',
-         ],
-      ];
+      // $dataSet[] = [
+      //    'instance' => $instance,
+      //    'data' => [
+      //    ],
+      //    'formanswer' => $formAnswer,
+      //    'expected' => [
+      //       'itilcategories_id' => '42',
+      //    ],
+      // ];
+      // $data = [];
+      // $expected = [
+      //    'itilcategories_id' => '42',
+      // ];
 
-      return $dataSet;
-   }
-
-   /**
-    * @dataProvider providerSetTargetCategory
-    */
-   public function testSetTargetCategory($instance, $data, $formanswer, $expected) {
-      $output = $instance->publicSetTargetCategory($data, $formanswer);
-      $this->integer((int) $output['itilcategories_id'])->isEqualTo($expected['itilcategories_id']);
+      // $output = $instance->publicSetTargetCategory($data, $formAnswer);
+      // $this->integer((int) $output['itilcategories_id'])->isEqualTo($expected['itilcategories_id']);
    }
 }
