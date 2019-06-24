0e15eac0d7136e4f5637d6c8dbe121d367109fb8
Thierry Bugier <tbugier@teclib.com>
2019-06-24 20:05:05 +0200

    fix: security when loading a class
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/linker.class.php
+++ inc/linker.class.php
@@ -29,6 +29,10 @@
  * ---------------------------------------------------------------------
  */
 
+if (!defined('GLPI_ROOT')) {
+   die("Sorry. You can't access this file directly");
+}
+
 class PluginFormcreatorLinker
 {
    private $imported = [];
