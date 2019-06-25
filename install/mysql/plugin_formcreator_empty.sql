73c2f2cceafd4196c0e88ceb170f12b370deb2ab
Thierry Bugier <tbugier@teclib.com>
2019-06-25 13:08:57 +0200

    fix(install): inconsistency between install and upgrade

--- install/mysql/plugin_formcreator_empty.sql
+++ install/mysql/plugin_formcreator_empty.sql
@@ -3,8 +3,8 @@
 
 CREATE TABLE IF NOT EXISTS `glpi_plugin_formcreator_answers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `plugin_formcreator_formanswers_id` int(11) NOT NULL,
-  `plugin_formcreator_questions_id` int(11) NOT NULL,
+  `plugin_formcreator_formanswers_id` int(11) NOT NULL DEFAULT '0',
+  `plugin_formcreator_questions_id` int(11) NOT NULL DEFAULT '0',
   `answer` text,
   PRIMARY KEY (`id`),
   INDEX `plugin_formcreator_formanswers_id` (`plugin_formcreator_formanswers_id`),
