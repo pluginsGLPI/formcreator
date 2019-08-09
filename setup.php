9a8b724bef4472d11370d1c2bc2b7bd25b9eda75
Thierry Bugier <tbugier@teclib.com>
2019-08-09 10:08:29 +0200

    build: disable oficial build flag

--- setup.php
+++ setup.php
@@ -35,7 +35,7 @@ define('PLUGIN_FORMCREATOR_VERSION', '2.9.0-nightly');
 // Schema version of this version
 define('PLUGIN_FORMCREATOR_SCHEMA_VERSION', '2.9');
 // is or is not an official release of the plugin
-define('PLUGIN_FORMCREATOR_IS_OFFICIAL_RELEASE', true);
+define('PLUGIN_FORMCREATOR_IS_OFFICIAL_RELEASE', false);
 
 // Minimal GLPI version, inclusive
 define ('PLUGIN_FORMCREATOR_GLPI_MIN_VERSION', '9.4');
