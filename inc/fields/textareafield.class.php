39310e5a2061d82acd3d1462c57754ed6e0d5df1
Thierry Bugier <tbugier@teclib.com>
2019-11-21 16:02:49 +0100

    fix(textareafield): convert pasted images into documents
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/fields/textareafield.class.php
+++ inc/fields/textareafield.class.php
@@ -85,6 +85,7 @@ class PluginFormcreatorTextareaField extends PluginFormcreatorTextField
             'rows'            => 5,
             'display'         => false,
             'enable_richtext' => $useRichText,
+            'enable_fileupload'=> true,
          ]);
          echo Html::scriptBlock("$(function() {
             pluginFormcreatorInitializeTextarea('$fieldName', '$rand');
@@ -142,6 +143,18 @@ class PluginFormcreatorTextareaField extends PluginFormcreatorTextField
    public function prepareQuestionInputForSave($input) {
       $this->value = str_replace('\r\n', "\r\n", $input['default_values']);
       return $input;
+   }
+
+   public function parseAnswerValues($input, $nonDestructive = false) {
+      $input = $this->question->addFiles(
+         $input,
+         [
+            'force_update'  => true,
+            'content_field' => 'formcreator_field_' . $this->question->getID(),
+         ]
+      );
+
+      return parent::parseAnswerValues($input, $nonDestructive);
    }
 
    public function getValueForTargetText($richText) {
