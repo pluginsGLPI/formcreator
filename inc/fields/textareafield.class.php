3d53b181cae0d43a2a5c0e36d1d3891001aa3750
Thierry Bugier <tbugier@teclib.com>
2020-01-14 22:07:44 +0100

    fix(textarea): workaround GLPI bug in 9.4.5
    see https://github.com/glpi-project/glpi/pull/6811
    
    close #1613
    
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/fields/textareafield.class.php
+++ inc/fields/textareafield.class.php
@@ -21,7 +21,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Formcreator. If not, see <http://www.gnu.org/licenses/>.
  * ---------------------------------------------------------------------
- * @copyright Copyright © 2011 - 2020 Teclib'
+ * @copyright Copyright © 2011 - 2019 Teclib'
  * @license   http://www.gnu.org/licenses/gpl.txt GPLv3+
  * @link      https://github.com/pluginsGLPI/formcreator/
  * @link      https://pluginsglpi.github.io/formcreator/
@@ -78,14 +78,17 @@ class PluginFormcreatorTextareaField extends PluginFormcreatorTextField
       $value        = nl2br($this->value);
       $html = '';
       $html .= Html::textarea([
-         'name'            => $fieldName,
-         'rand'            => $rand,
-         'value'           => $value,
-         'rows'            => 5,
-         'display'         => false,
-         'enable_richtext' => true,
-         'enable_fileupload'=> true,
+         'name'              => $fieldName,
+         'rand'              => $rand,
+         'value'             => $value,
+         'rows'              => 5,
+         'display'           => false,
+         'enable_richtext'   => true,
+         'enable_fileupload' => true,
       ]);
+         if (version_compare(GLPI_VERSION, '9.4.6') < 0) {
+            $html .= '</div>';
+         }
       $html .= Html::scriptBlock("$(function() {
          pluginFormcreatorInitializeTextarea('$fieldName', '$rand');
       });");
