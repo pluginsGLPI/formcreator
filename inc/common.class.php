fb64ee555b03ee4c89df75e486b4ce75029af4c2
Thierry Bugier <tbugier@teclib.com>
2020-01-30 11:17:00 +0100

    refactor(targetticket): remove workaround
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/common.class.php
+++ inc/common.class.php
@@ -83,35 +83,6 @@ class PluginFormcreatorCommon {
    }
 
    /**
-    * Get Link Name
-    *
-    * @param integer $value    Current value
-    * @param boolean $inverted Whether to invert label
-    *
-    * @return string
-    */
-   public static function getLinkName($value, $inverted = false) {
-      $tmp = [];
-
-      if (!$inverted) {
-         $tmp[Ticket_Ticket::LINK_TO]        = __('Linked to');
-         $tmp[Ticket_Ticket::DUPLICATE_WITH] = __('Duplicates');
-         $tmp[Ticket_Ticket::SON_OF]         = __('Son of');
-         $tmp[Ticket_Ticket::PARENT_OF]      = __('Parent of');
-      } else {
-         $tmp[Ticket_Ticket::LINK_TO]        = __('Linked to');
-         $tmp[Ticket_Ticket::DUPLICATE_WITH] = __('Duplicated by');
-         $tmp[Ticket_Ticket::SON_OF]         = __('Parent of');
-         $tmp[Ticket_Ticket::PARENT_OF]      = __('Son of');
-      }
-
-      if (isset($tmp[$value])) {
-         return $tmp[$value];
-      }
-      return NOT_AVAILABLE;
-   }
-
-   /**
     * Gets the ID of Formcreator request type
     */
    public static function getFormcreatorRequestTypeId() {
