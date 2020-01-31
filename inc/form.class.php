818eab0a22f5bc2bfa95b9fe32399bc594ddf5a9
Thierry Bugier <tbugier@teclib.com>
2020-01-31 17:22:48 +0100

    fix(form,question): duplicate fail on form without section
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/form.class.php
+++ inc/form.class.php
@@ -2018,11 +2018,15 @@ PluginFormcreatorConditionnableInterface
             }
             $importedItems[] = $importedItem;
          }
-         // Delete all other restrictions
+         // Delete all other sections
+         $deleteCriteria = [];
+         if (count($importedItems) > 0) {
+            $deleteCriteria = ['NOT' => ['id' => $importedItems]];
+         }
          $FormProfile = new PluginFormcreatorSection();
          $FormProfile->deleteByCriteria([
             $formFk => $itemId,
-            ['NOT' => ['id' => $importedItems]]
+            $deleteCriteria,
          ]);
       }
 
