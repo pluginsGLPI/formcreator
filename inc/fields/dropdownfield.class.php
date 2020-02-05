da0aa81035c712a812fc0186476e082fd3ebff88
Thierry Bugier <tbugier@teclib.com>
2020-02-05 12:24:14 +0100

    fix(glpiselectfield): restrict to items associatable to tickets > > restriction on per item basis, like software
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/fields/dropdownfield.class.php
+++ inc/fields/dropdownfield.class.php
@@ -242,7 +242,13 @@ class PluginFormcreatorDropdownField extends PluginFormcreatorField
                      ];
                      $groups = implode("', '", $groups);
                   }
-               }
+                  // Check if helpdesk availability is fine tunable on a per item basis
+                  if ($DB->fieldExists($itemtype::getTable(), 'is_helpdesk_visible')) {
+                     $dparams_cond_crit[] = [
+                        'is_helpdesk_visible' => '1',
+                     ];
+                  }
+            }
          }
 
          // Set specific root if defined (CommonTreeDropdown)
