948dddee12996fa081f48a23187768d5aaed8aaa
Thierry Bugier <tbugier@teclib.com>
2020-03-05 16:14:01 +0100

    fix(form): hidden questions still consume 10 pixels height
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/form.class.php
+++ inc/form.class.php
@@ -1864,7 +1864,7 @@ PluginFormcreatorConditionnableInterface
       global $DB;
 
       if (!isset($input['uuid']) && !isset($input['id'])) {
-         throw new ImportFailureException('UUID or ID is mandatory');
+         throw new ImportFailureException('');
       }
 
       $formFk = PluginFormcreatorForm::getForeignKeyField();
