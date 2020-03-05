81bc7b6bf6d334c871cb92b0ec07a8893857b157
Thierry Bugier <tbugier@teclib.com>
2020-03-05 16:38:32 +0100

    fix: remove code left for debug
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/form.class.php
+++ inc/form.class.php
@@ -1896,7 +1896,7 @@ PluginFormcreatorConditionnableInterface
       global $DB;
 
       if (!isset($input['uuid']) && !isset($input['id'])) {
-         throw new ImportFailureException('');
+         throw new ImportFailureException('UUID or ID is mandatory');
       }
 
       $formFk = PluginFormcreatorForm::getForeignKeyField();
