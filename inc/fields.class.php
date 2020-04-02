9e6ae32bf863aa6d328f7f7f85696ef7bad6fbd0
Thierry Bugier <tbugier@teclib.com>
2020-04-02 11:18:43 +0200

    fix(condition): php warning if a wuestion does not exists
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/fields.class.php
+++ inc/fields.class.php
@@ -189,7 +189,10 @@ class PluginFormcreatorFields
             $nextLogic = PluginFormcreatorCondition::SHOW_LOGIC_OR;
          }
 
-         // TODO: find the best behavior if the question does not exists
+         if (!isset($fields[$condition['field']])) {
+            // The field does not exists, give up and make the field visible
+            return true;
+         }
          $conditionField = $fields[$condition['field']];
 
          $value = false;
