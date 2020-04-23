e3ecf02a097734523f83ba638fabdb19d9f7bc72
Thierry Bugier <tbugier@teclib.com>
2020-04-23 14:00:27 +0200

    feat(section): make section title required
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/section.class.php
+++ inc/section.class.php
@@ -396,7 +396,7 @@ PluginFormcreatorConditionnableInterface
       echo '<tr>';
       echo '<td width="20%">'.__('Title').' <span style="color:red;">*</span></td>';
       echo '<td colspan="3">';
-      echo Html::input('name', ['style' => 'width: calc(100% - 20px)', 'value' => $this->fields['name']]);
+      echo Html::input('name', ['style' => 'width: calc(100% - 20px)', 'required' => 'required', 'value' => $this->fields['name']]);
       echo '</td>';
       echo '</tr>';
 
