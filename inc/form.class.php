fb37c460cc08e1bd75b5a69daa6aa252b86266bc
Thierry Bugier <tbugier@teclib.com>
2020-04-28 11:35:55 +0200

    fix(form): validators must show when more than 2 available
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/form.class.php
+++ inc/form.class.php
@@ -1248,19 +1248,17 @@ PluginFormcreatorConditionnableInterface
                break;
          }
 
-         switch (count($result)) {
-            case 1:
-               reset($validators);
-               $validatorId = key($validators);
-               echo Html::hidden('formcreator_validator', ['value' => $validatorId]);
-               break;
-            case 2:
-               $validators = [0 => Dropdown::EMPTY_VALUE] + $validators;
-               echo '<h2>' . __('Validation', 'formcreator') . '</h2>';
-               echo '<div class="form-group required liste" id="form-validator">';
-               echo '<label>' . __('Choose a validator', 'formcreator') . ' <span class="red">*</span></label>';
-               Dropdown::showFromArray('formcreator_validator', $validators);
-               break;
+         $resultCount = count($result);
+         if ($resultCount == 1) {
+            reset($validators);
+            $validatorId = key($validators);
+            echo Html::hidden('formcreator_validator', ['value' => $validatorId]);
+         } else if ($resultCount > 1) {
+            $validators = [0 => Dropdown::EMPTY_VALUE] + $validators;
+            echo '<h2>' . __('Validation', 'formcreator') . '</h2>';
+            echo '<div class="form-group required liste" id="form-validator">';
+            echo '<label>' . __('Choose a validator', 'formcreator') . ' <span class="red">*</span></label>';
+            Dropdown::showFromArray('formcreator_validator', $validators);
          }
       }
 
