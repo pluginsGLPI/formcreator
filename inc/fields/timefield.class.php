d08cc0b8bac2e59b5fb7dd0f7ae0f55686710b6d
Thierry Bugier <tbugier@teclib.com>
2020-04-28 16:14:54 +0200

    fix(timefield): assign a default value
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/fields/timefield.class.php
+++ inc/fields/timefield.class.php
@@ -35,6 +35,43 @@ class PluginFormcreatorTimeField extends PluginFormcreatorField
       return true;
    }
 
+   public function getDesignSpecializationField() {
+      $rand = mt_rand();
+
+      $label = '';
+      $field = '';
+
+      $additions = '<tr class="plugin_formcreator_question_specific">';
+      $additions .= '<td>';
+      $additions .= '<label for="dropdown_default_values'.$rand.'">';
+      $additions .= __('Default values');
+      $additions .= '</label>';
+      $additions .= '</td>';
+      $additions .= '<td>';
+      $value = Html::entities_deep($this->question->fields['default_values']);
+      $additions .= Html::showTimeField('default_values', [
+         'type'    => 'text',
+         'id'      => 'default_values',
+         'value'   => $value,
+         'display' => false,
+      ]);
+      $additions .= '</td>';
+      $additions .= '<td></td>';
+      $additions .= '<td></td>';
+      $additions .= '</tr>';
+
+      $common = parent::getDesignSpecializationField();
+      $additions .= $common['additions'];
+
+      return [
+         'label' => $label,
+         'field' => $field,
+         'additions' => $additions,
+         'may_be_empty' => false,
+         'may_be_required' => true,
+      ];
+   }
+
    public function getRenderedHtml($canEdit = true) {
       if (!$canEdit) {
          return $this->value;
