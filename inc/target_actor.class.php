6e0596274131b0f7e76d1a5af7a227cbe55d23ef
Thierry Bugier <tbugier@teclib.com>
2020-06-12 16:42:50 +0200

    fix(target_actor): bad key for users, groups or suppliers
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/target_actor.class.php
+++ inc/target_actor.class.php
@@ -134,7 +134,7 @@ abstract class PluginFormcreatorTarget_Actor extends CommonDBChild implements Pl
 
          case self::ACTOR_TYPE_PERSON:
             $user = new User;
-            $users_id = plugin_formcreator_getFromDBByField($user, 'name', $input['_user']);
+            $users_id = plugin_formcreator_getFromDBByField($user, 'name', $input['actor_value']);
             if ($users_id === false) {
                throw new ImportFailureException('failed to find a user');
             }
@@ -143,7 +143,7 @@ abstract class PluginFormcreatorTarget_Actor extends CommonDBChild implements Pl
 
          case self::ACTOR_TYPE_GROUP:
             $group = new Group;
-            $groups_id = plugin_formcreator_getFromDBByField($group, 'completename', $input['_group']);
+            $groups_id = plugin_formcreator_getFromDBByField($group, 'completename', $input['actor_value']);
             if ($groups_id === false) {
                throw new ImportFailureException('failed to find a group');
             }
@@ -152,7 +152,7 @@ abstract class PluginFormcreatorTarget_Actor extends CommonDBChild implements Pl
 
          case self::ACTOR_TYPE_SUPPLIER:
             $supplier = new Supplier;
-            $suppliers_id = plugin_formcreator_getFromDBByField($supplier, 'name', $input['_supplier']);
+            $suppliers_id = plugin_formcreator_getFromDBByField($supplier, 'name', $input['actor_value']);
             if ($suppliers_id === false) {
                throw new ImportFailureException('failed to find a supplier');
             }
