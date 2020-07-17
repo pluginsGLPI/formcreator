2947d6f683dae27f854bdd1500322f281c8ea7e3
Thierry Bugier <tbugier@teclib.com>
2020-07-17 10:18:02 +0200

    fix(form): forbid clone massive action in GLPI 9.5
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/form.class.php
+++ inc/form.class.php
@@ -1532,6 +1532,12 @@ PluginFormcreatorConditionnableInterface
       return $result;
    }
 
+   public function getForbiddenStandardMassiveAction() {
+      return [
+         'clone',
+      ];
+   }
+
    /**
     * @since version 0.85
     *
