4165e3af13a39a12f8d42a6a3eb76257a0438bef
Thierry Bugier <tbugier@teclib.com>
2020-07-23 11:09:19 +0200

    refactor(form_profile): use conventional foreign key
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- front/form_profile.form.php
+++ front/form_profile.form.php
@@ -39,26 +39,26 @@ if (!$plugin->isActivated("formcreator")) {
    Html::displayNotFoundError();
 }
 
-if (isset($_POST["profiles_id"]) && isset($_POST["form_id"])) {
-
+$formFk = PluginFormcreatorForm::getForeignKeyField();
+if (isset($_POST["profiles_id"]) && isset($_POST[$formFk])) {
    if (isset($_POST['access_rights'])) {
       $form = new PluginFormcreatorForm();
       $form->update([
-         'id'            => (int) $_POST['form_id'],
+         'id'            => (int) $_POST[$formFk],
          'access_rights' => (int) $_POST['access_rights']
       ]);
    }
 
    $form_profile = new PluginFormcreatorForm_Profile();
    $form_profile->deleteByCriteria([
-         'plugin_formcreator_forms_id'    => (int) $_POST["form_id"],
+         $formFk    => (int) $_POST[$formFk],
    ]);
 
    foreach ($_POST["profiles_id"] as $profile_id) {
       if ($profile_id != 0) {
          $form_profile = new PluginFormcreatorForm_Profile();
          $form_profile->add([
-               'plugin_formcreator_forms_id' => (int) $_POST["form_id"],
+               'plugin_formcreator_forms_id' => (int) $_POST[$formFk],
                'profiles_id'                 => (int) $profile_id,
          ]);
       }
