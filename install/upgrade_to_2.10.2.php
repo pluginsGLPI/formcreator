90727ae21ed3986c3c84a477bae0273a129872e7
Thierry Bugier <tbugier@teclib.com>
2020-08-11 17:54:59 +0200

    fix(issue): repopulate table on upgrade
    counters were improved and data in the table must be rebuild
    
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

---
+++ install/upgrade_to_2.10.2.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * ---------------------------------------------------------------------
+ * Formcreator is a plugin which allows creation of custom forms of
+ * easy access.
+ * ---------------------------------------------------------------------
+ * LICENSE
+ *
+ * This file is part of Formcreator.
+ *
+ * Formcreator is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Formcreator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Formcreator. If not, see <http://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------
+ * @copyright Copyright © 2011 - 2019 Teclib'
+ * @license   http://www.gnu.org/licenses/gpl.txt GPLv3+
+ * @link      https://github.com/pluginsGLPI/formcreator/
+ * @link      https://pluginsglpi.github.io/formcreator/
+ * @link      http://plugins.glpi-project.org/#/plugin/formcreator
+ * ---------------------------------------------------------------------
+ */
+class PluginFormcreatorUpgradeTo2_10_2 {
+
+   protected $migration;
+
+   /**
+    * @param Migration $migration
+    */
+   public function upgrade(Migration $migration) {
+      global $DB;
+
+      $this->migration = $migration;
+
+      // Versioin 2.10.2 contains fixes on counters requiring repopulation of issues table
+      $table = 'glpi_plugin_formcreator_issues';
+      $DB->query("TRUNCATE `$table`");
+      PluginFormcreatorIssue::cronSyncIssues(new CronTask());
+   }
+}
