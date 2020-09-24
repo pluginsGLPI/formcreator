8a26b77a0dfc164ff2906ee5fce8f3561b51b4d3
Thierry Bugier <tbugier@teclib.com>
2020-09-24 09:04:21 +0200

    refactor(issue): reduce code complexity
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- front/issue.php
+++ front/issue.php
@@ -37,32 +37,31 @@ if (!$plugin->isActivated('formcreator')) {
    Html::displayNotFoundError();
 }
 
-if (PluginFormcreatorIssue::canView()) {
-   if (plugin_formcreator_replaceHelpdesk()) {
-      PluginFormcreatorWizard::header(__('Service catalog', 'formcreator'));
-   } else {
-      if ($_SESSION['glpiactiveprofile']['interface'] == 'helpdesk') {
-         Html::helpHeader(
-            __('Form Creator', 'formcreator'),
-            $_SERVER['PHP_SELF']
-         );
-      } else {
-         Html::header(
-            __('Form Creator', 'formcreator'),
-            $_SERVER['PHP_SELF'],
-            'helpdesk',
-            'PluginFormcreatorFormlist'
-         );
-      }
-   }
-
-   Search::show('PluginFormcreatorIssue');
-
-   if (plugin_formcreator_replaceHelpdesk()) {
-      PluginFormcreatorWizard::footer();
-   } else {
-      Html::footer();
-   }
-} else {
+if (!PluginFormcreatorIssue::canView()) {
    Html::displayRightError();
+}
+if (plugin_formcreator_replaceHelpdesk()) {
+   PluginFormcreatorWizard::header(__('Service catalog', 'formcreator'));
+} else {
+   if ($_SESSION['glpiactiveprofile']['interface'] == 'helpdesk') {
+      Html::helpHeader(
+         __('Form Creator', 'formcreator'),
+         $_SERVER['PHP_SELF']
+      );
+   } else {
+      Html::header(
+         __('Form Creator', 'formcreator'),
+         $_SERVER['PHP_SELF'],
+         'helpdesk',
+         'PluginFormcreatorFormlist'
+      );
+   }
+}
+
+Search::show('PluginFormcreatorIssue');
+
+if (plugin_formcreator_replaceHelpdesk()) {
+   PluginFormcreatorWizard::footer();
+} else {
+   Html::footer();
 }
