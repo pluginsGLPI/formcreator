636d674a713b6f3f975e458f4cb4a991da7ef5ec
Thierry Bugier <tbugier@teclib.com>
2020-10-20 09:24:07 +0200

    fix(form): missinb closing tag
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/form.class.php
+++ inc/form.class.php
@@ -1291,6 +1291,7 @@ PluginFormcreatorConditionnableInterface
             echo '<div class="form-group required liste" id="form-validator">';
             echo '<label>' . __('Choose a validator', 'formcreator') . ' <span class="red">*</span></label>';
             Dropdown::showFromArray('formcreator_validator', $validators);
+            echo '</div>';
          }
       }
 
