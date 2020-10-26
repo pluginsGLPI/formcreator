1249f9f26e1debc1faa3f06f29f21c2b76f02326
Adrien Clairembault <42734840+AdrienClairembault@users.noreply.github.com>
2020-10-26 09:15:56 +0100

    New syncissues command + fix sync issues cron (#1984)
    feat: CLI command to force Sync Issues

---
+++ inc/command/syncissuescommand.class.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ -------------------------------------------------------------------------
+ location plugin for GLPI
+ Copyright (C) 2020 by the location Development Team.
+
+ https://github.com/pluginsGLPI/location
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of location.
+
+ location is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ location is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with location. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
+
+namespace GlpiPlugin\Formcreator\Command;
+
+use PluginFormcreatorIssue;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SyncIssuesCommand extends Command
+{
+   protected function configure() {
+      $table = PluginFormcreatorIssue::getTable();
+
+      $this
+         ->setName('glpi:plugins:formcreator:syncissues')
+         ->setDescription("Rebuild `$table` table.");
+   }
+
+   protected function execute(InputInterface $input, OutputInterface $output) {
+      global $DB;
+
+      $output->write("<info>-> Step 1/2...</info>");
+      $DB->delete(PluginFormcreatorIssue::getTable(), [1]);
+      $output->write('<info> OK.</info>');
+      $output->writeln("");
+
+      $output->write("<info>-> Step 2/2...</info>");
+      PluginFormcreatorIssue::syncIssues();
+      $output->write('<info> OK.</info>');
+      $output->writeln("");
+
+      $output->writeln('<info>Done.</info>');
+      return 0;
+   }
+}
