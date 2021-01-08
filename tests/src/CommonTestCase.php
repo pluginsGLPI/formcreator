d91cd497dfda552e85a1602ed2119544deab0e61
Thierry Bugier <tbugier@teclib.com>
2021-01-08 15:31:02 +0100

    test: better assertions
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- tests/src/CommonTestCase.php
+++ tests/src/CommonTestCase.php
@@ -318,6 +318,7 @@ abstract class CommonTestCase extends CommonDBTestCase
       if (!is_array($message)) {
          $message = [$message];
       }
+      $this->array($_SESSION['MESSAGE_AFTER_REDIRECT'])->hasKey($message_type);
       $this->array($_SESSION['MESSAGE_AFTER_REDIRECT'][$message_type])
          ->containsValues($message, $this->getSessionMessage());
    }
