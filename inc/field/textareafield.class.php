391025f375d6306361e20e8a07a7697b9c6108f6
Thierry Bugier <tbugier@teclib.com>
2021-01-15 13:28:57 +0100

    docs(textareafield): malformed phpdoc
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/field/textareafield.class.php
+++ inc/field/textareafield.class.php
@@ -39,7 +39,7 @@ use Toolbox;
 
 class TextareaField extends TextField
 {
-   /** @var $uploads array uploaded files on form submit */
+   /** @var array uploaded files on form submit */
    private $uploads = [
       '_filename' => [],
       '_prefix_filename' => [],
