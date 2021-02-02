77d3a3293952193eb3d472c69c90ac4485c056ae
Thierry Bugier <tbugier@teclib.com>
2021-02-02 09:59:02 +0100

    fix(issue): update handling of url in emai: notifications
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- setup.php
+++ setup.php
@@ -188,7 +188,15 @@ function plugin_init_formcreator() {
                   if (isset($decodedUrl['forcetab'])) {
                      Session::setActiveTab(Ticket::class, $decodedUrl['forcetab']);
                   }
-                  Html::redirect(FORMCREATOR_ROOTDOC . '/front/issue.form.php?id=' . $_GET['id'] . '&sub_itemtype=Ticket' . $openItilFollowup);
+                  $issue = new PluginFormcreatorIssue();
+                  $issue->getFromDBByCrit([
+                     'sub_itemtype' => Ticket::class,
+                     'original_id'  => (int) $_GET['id']
+                  ]);
+                  if ($issue->isNewItem()) {
+                     Html::displayNotFoundError();
+                  }
+                  Html::redirect($issue->getFormURLWithID($issue->getID()) . $openItilFollowup);
                }
             }
 
