2bb76333e933532242bcfecae0220237513b808f
Thierry Bugier <tbugier@teclib.com>
2021-02-18 09:35:07 +0100

    fix(wizard): don't show tabs for KB item in service catalog
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- front/knowbaseitem.form.php
+++ front/knowbaseitem.form.php
@@ -52,7 +52,7 @@ if (isset($_GET["id"])) {
       }
    }
    $_SESSION['glpilisturl']['KnowbaseItem'] = FORMCREATOR_ROOTDOC."/front/wizard.php";
-   $kb->display($options);
+   $kb->showFull($options);
 
    PluginFormcreatorWizard::footer();
 }
