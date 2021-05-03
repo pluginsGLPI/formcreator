619f647cf96e4df07f9130bd616a0fb10bce05e6
Thierry Bugier <tbugier@teclib.com>
2021-05-03 09:07:55 +0200

    fix(install): port DATETIME to TIMESTAMP upgrade bug from glpi
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

---
+++ install/upgrade_to_2.11.3.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * ---------------------------------------------------------------------
+ * Formcreator is a plugin which allows creation of custom forms of
+ * easy access.
+ * ---------------------------------------------------------------------
+ * LICENSE
+ *
+ * This file is part of Formcreator.
+ *
+ * Formcreator is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Formcreator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Formcreator. If not, see <http://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------
+ * @copyright Copyright © 2011 - 2021 Teclib'
+ * @license   http://www.gnu.org/licenses/gpl.txt GPLv3+
+ * @link      https://github.com/pluginsGLPI/formcreator/
+ * @link      https://pluginsglpi.github.io/formcreator/
+ * @link      http://plugins.glpi-project.org/#/plugin/formcreator
+ * ---------------------------------------------------------------------
+ */
+class PluginFormcreatorUpgradeTo2_11_3 {
+   /** @var Migration */
+   protected $migration;
+
+   /**
+    * @param Migration $migration
+    */
+   public function upgrade(Migration $migration) {
+      global $DB;
+
+      $this->migration = $migration;
+
+      // Convert datetime to timestamp
+      $table = 'glpi_plugin_formcreator_formanswers';
+      $migration->changeField($table, 'request_date', 'request_date', 'datetime'. ' NOT NULL');
+
+      $table = 'glpi_plugin_formcreator_issues';
+      $migration->changeField($table, 'date_creation', 'date_creation', 'datetime'. ' NOT NULL');
+      $migration->changeField($table, 'date_mod', 'date_mod', 'datetime'. ' NOT NULL');
+   }
+}
