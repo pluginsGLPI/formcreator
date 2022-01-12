9f318a31112026ccd63f7843e4e7e966cbd8e637
AdrienClairembault <aclairembault@teclib.com>
2022-01-12 11:18:53 +0100

    Take users_id_recipient into account

--- install/mysql/plugin_formcreator_empty.sql
+++ install/mysql/plugin_formcreator_empty.sql
@@ -254,6 +254,7 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_formcreator_issues` (
   `requester_id`          int(11)       NOT NULL DEFAULT '0',
   `users_id_validator`    int(11)       NOT NULL DEFAULT '0',
   `groups_id_validator`   int(11)       NOT NULL DEFAULT '0',
+  `users_id_recipient`    int(11)        NOT NULL DEFAULT '0',
   `comment`               longtext,
   PRIMARY KEY (`id`),
   INDEX `item` (`itemtype`, `items_id`),
