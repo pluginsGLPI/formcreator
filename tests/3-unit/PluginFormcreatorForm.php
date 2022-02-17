3ecbd47bb2fcb2565e79e9c0836a5da98de604bc
Thierry Bugier <tbugier@teclib.com>
2022-02-17 13:55:27 +0100

    test(form): add tests to count forms

--- tests/3-unit/PluginFormcreatorForm.php
+++ tests/3-unit/PluginFormcreatorForm.php
@@ -43,6 +43,7 @@ class PluginFormcreatorForm extends CommonTestCase {
          case 'testDuplicate':
          case 'testCreateValidationNotification':
          case 'testGetTranslatableStrings':
+         case 'testCountAvailableForm':
             $this->login('glpi', 'glpi');
       }
    }
@@ -1312,5 +1313,71 @@ class PluginFormcreatorForm extends CommonTestCase {
       $this->array($rows)->hasSize(1);
       $row = array_shift($rows);
       $this->string($row['name'])->isEqualTo(\PluginFormcreatorSection::getTypeName(1));
+   }
+
+   /**
+    *
+    */
+   public function testCountAvailableForm() {
+      $entity = new \Entity();
+      $entityId = $entity->import([
+         'entities_id' => '0',
+         'name' => $this->getUniqueString()
+      ]);
+
+      $this->boolean(\Session::changeActiveEntities($entityId))->isTrue();
+
+      $form1 = $this->getForm([
+         'entities_id' => $entityId,
+         'is_visible'  => 1,
+         'is_deleted'  => 0,
+         'is_active'   => 1,
+         'language'    => ''
+      ]);
+
+      $form2 = $this->getForm([
+         'entities_id' => $entityId,
+         'is_visible'  => 1,
+         'is_deleted'  => 0,
+         'is_active'   => 1,
+         'language'    => $_SESSION['glpilanguage'],
+      ]);
+
+      // All nextform should not be counted
+      $form3 = $this->getForm([
+         'entities_id' => $entityId,
+         'is_visible'  => 0,
+         'is_deleted'  => 0,
+         'is_active'   => 1,
+         'language'    => '',
+      ]);
+
+      $form4 = $this->getForm([
+         'entities_id' => $entityId,
+         'is_visible'  => 0,
+         'is_deleted'  => 0,
+         'is_active'   => 0,
+         'language'    => '',
+      ]);
+
+      $form5 = $this->getForm([
+         'entities_id' => $entityId,
+         'is_visible'  => 0,
+         'is_deleted'  => 1,
+         'is_active'   => 0,
+         'language'    => '',
+      ]);
+
+      $form6 = $this->getForm([
+         'entities_id' => $entityId,
+         'is_visible'  => 0,
+         'is_deleted'  => 1,
+         'is_active'   => 0,
+         'language'    => 'de_DE',
+      ]);
+
+      $output = \PluginFormcreatorForm::countAvailableForm();
+
+      $this->integer($output)->isEqualTo(2);
    }
 }
