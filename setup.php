a44fefd83e9fd372823bf9ea86e3c95fa361d0d5
Thierry Bugier <tbugier@teclib.com>
2022-02-18 10:13:13 +0100

    bump(2.12.5): version 2.12.5

--- setup.php
+++ setup.php
@@ -31,7 +31,7 @@
 
 global $CFG_GLPI;
 // Version of the plugin (major.minor.bugfix)
-define('PLUGIN_FORMCREATOR_VERSION', '2.12.4');
+define('PLUGIN_FORMCREATOR_VERSION', '2.12.5');
 // Schema version of this version (major.minor only)
 define('PLUGIN_FORMCREATOR_SCHEMA_VERSION', '2.12');
 // is or is not an official release of the plugin
