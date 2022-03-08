cc25fb2df8a2779016138912243f7a80d509da19
Thierry Bugier <tbugier@teclib.com>
2022-03-08 13:03:40 +0100

    feat(form): icon dropdown list included in the project
    svg no longer in  Font Awesome 6; an API is provided to get data we need, but it timeouts too often with 5 seconds

--- inc/common.class.php
+++ inc/common.class.php
@@ -157,10 +157,10 @@ class PluginFormcreatorCommon {
     *
     * @return array
     */
-   public static function getFontAwesomePictoNames() : array {
+   public static function getFontAwesomePictoNames(): array {
       static $list = null;
 
-      $list = $list ?? require_once(GLPI_PLUGIN_DOC_DIR . '/formcreator/' . self::getPictoFilename());
+      $list = $list ?? require_once(Plugin::getPhpDir('formcreator') . '/data/' . self::getPictoFilename());
       return $list;
    }
 
@@ -539,39 +539,6 @@ JAVASCRIPT;
             }
          }
       }
-   }
-
-   public static function buildFontAwesomeData() {
-      $fontAwesomeDir = GLPI_ROOT . '/public/lib/fortawesome/fontawesome-free/webfonts';
-      $outFile = GLPI_PLUGIN_DOC_DIR . '/formcreator/font-awesome.php';
-      @mkdir(dirname($outFile));
-      if (!is_readable($fontAwesomeDir) || !is_writable(dirname($outFile))) {
-         return false;
-      }
-
-      $faSvgFiles = [
-            'fa' => "$fontAwesomeDir/fa-regular-400.svg",
-            'fab' => "$fontAwesomeDir/fa-brands-400.svg",
-            'fas' => "$fontAwesomeDir/fa-solid-900.svg",
-         ];
-
-      $fanames = [];
-      $searchRegex = '#glyph-name=\"([^\"]*)\"#i';
-      foreach ($faSvgFiles as $key => $svgSource) {
-         $svg = file_get_contents($svgSource);
-         $matches = null;
-         preg_match_all($searchRegex, $svg, $matches);
-         foreach ($matches[1] as $name) {
-            $fanames["$key fa-$name"] = $name;
-         }
-         $list = '<?php' . PHP_EOL . 'return ' . var_export($fanames, true) . ';';
-         $size = file_put_contents($outFile, $list);
-         if ($size != strlen($list)) {
-            return false;
-         }
-      }
-
-      return true;
    }
 
    /**
--- tests/1-install/Config.php
+++ tests/1-install/Config.php
@@ -124,7 +124,6 @@ class Config extends CommonTestCase {
 
       // Check the version saved in configuration
       $this->checkConfig();
-      $this->checkFontAwesomeData();
       $this->testPluginName();
 
       $fresh_tables = $DB->listTables("glpi_plugin_${pluginName}_%");
@@ -186,13 +185,6 @@ class Config extends CommonTestCase {
       ]);
       $this->boolean($cronTask->isNewItem())->isFalse();
       $this->integer((int) $cronTask->fields['state'])->isEqualTo(0);
-   }
-
-   public function checkFontAwesomeData() {
-      $pluginName = TEST_PLUGIN_NAME;
-
-      $file = GLPI_ROOT . '/files/_plugins/' . $pluginName . '/font-awesome.php';
-      $this->boolean(is_readable($file))->isTrue();
    }
 
    /**
