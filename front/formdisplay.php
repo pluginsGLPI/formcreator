c76d91ea5a50e58e935affc8d0d4114ad1dad606
btry <tbugier@teclib.com>
2022-04-28 14:19:08 +0200

    refactor: use GLPI api

--- front/formdisplay.php
+++ front/formdisplay.php
@@ -53,7 +53,7 @@ if (isset($_REQUEST['id'])
    }
 
    // If the form has restriced access and user is not logged in, send to login form
-   if ($form->fields['access_rights'] == PluginFormcreatorForm::ACCESS_RESTRICTED && !isset($_SESSION['glpiID'])) {
+   if ($form->fields['access_rights'] == PluginFormcreatorForm::ACCESS_RESTRICTED && Session::getLoginUserID() === false) {
       Session::redirectIfNotLoggedIn();
       exit();
    }
