681be8499712fb7803f6fdcbcc088ae7bb3c2b4b
Stanislas KITA <skita@teclib.com>
2022-05-05 14:45:11 +0200

    fix(update): check if field not exist

--- install/upgrade_to_2.13.php
+++ install/upgrade_to_2.13.php
@@ -52,10 +52,13 @@ class PluginFormcreatorUpgradeTo2_13 {
    }
 
    public function addEntityOption() {
+      global $DB;
       $table = 'glpi_plugin_formcreator_entityconfigs';
 
-      $this->migration->addField($table, 'is_search_issue_visible', 'integer', ['after' => 'is_header_visible', 'value' => '-2']);
-      $this->migration->addPostQuery("UPDATE `glpi_plugin_formcreator_entityconfigs` SET `is_search_issue_visible`= 1 WHERE `entities_id` = 0");
+      if (!$DB->fieldExists($table, 'is_search_issue_visible')) {
+         $this->migration->addField($table, 'is_search_issue_visible', 'integer', ['after' => 'is_header_visible', 'value' => '-2']);
+         $this->migration->addPostQuery("UPDATE `glpi_plugin_formcreator_entityconfigs` SET `is_search_issue_visible`= 1 WHERE `entities_id` = 0");
+      }
    }
 
    public function addFormAnswerTitle() {
