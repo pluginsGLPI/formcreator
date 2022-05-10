c2ed8bf9db394951e66fd61a15679f3beab9d230
Stanislas KITA <skita@teclib.com>
2022-05-10 08:57:24 +0200

    remove useless debug code

--- js/scripts.js
+++ js/scripts.js
@@ -387,7 +387,6 @@ function buildTiles(list) {
          }
 
          var tiles_design = "";
-         console.log(item.tile_template);
          if (item.tile_template == "1") { // @see PluginFormcreatorEntityConfig::CONFIG_UI_FORM_UNIFORM_HEIGHT
             tiles_design = "tile_design_uniform_height";
          }
