d8d10cf5ce74ff9eeb2f4ee70fd2f7681252852e
Thierry Bugier <tbugier@teclib.com>
2022-07-06 15:46:57 +0200

    chore(2.13.0-dev): 2.13.0-dev

--- setup.php
+++ setup.php
@@ -33,7 +33,7 @@ use Glpi\Plugin\Hooks;
 
 global $CFG_GLPI;
 // Version of the plugin (major.minor.bugfix)
-define('PLUGIN_FORMCREATOR_VERSION', '2.13.0-rc.1');
+define('PLUGIN_FORMCREATOR_VERSION', '2.13.0-dev');
 // Schema version of this version (major.minor only)
 define('PLUGIN_FORMCREATOR_SCHEMA_VERSION', '2.13');
 // is or is not an official release of the plugin
