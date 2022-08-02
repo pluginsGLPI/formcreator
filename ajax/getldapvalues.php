d8b3fb412d7901dd29b2f6c6259a14766a4da557
Thierry Bugier <tbugier@teclib.com>
2022-08-02 23:54:06 +0200

    feat(ldapselectfield): lazy loading

--- ajax/getldapvalues.php
+++ ajax/getldapvalues.php
@@ -31,18 +31,17 @@
 
 include ('../../../inc/includes.php');
 
-// Direct access to file
-if (strpos($_SERVER['PHP_SELF'], 'gettranslationsvalues.php')) {
-   header('Content-Type: application/json; charset=UTF-8');
-   Html::header_nocache();
-} else if (!defined('GLPI_ROOT')) {
+if (!defined('GLPI_ROOT')) {
    die("Sorry. You can't access this file directly");
 }
 
+header('Content-Type: application/json; charset=UTF-8');
+Html::header_nocache();
+
 // Check if plugin is activated...
-if (!(new Plugin())->isActivated('formcreator')) {
+if (!Plugin::isPluginActive('formcreator')) {
    Html::displayNotFoundError();
 }
 
 Session::checkLoginUser();
-echo PluginFormcreatorTranslation::getDropdownValue($_POST);
+echo PluginFormcreatorLdapDropdown::getDropdownValue($_POST);
