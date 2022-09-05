ec08d95e02090c29af61768699b16721d60afbde
VladoTTX <71901245+VladoTTX@users.noreply.github.com>
2022-09-05 17:44:35 +0200

    fix(targetticket,targetchange): tags from queestion or specific tags not saved

--- inc/targetchange.class.php
+++ inc/targetchange.class.php
@@ -580,14 +580,13 @@ class PluginFormcreatorTargetChange extends PluginFormcreatorAbstractItilTarget
             }
          }
 
-         $plugin = new Plugin();
-         if ($plugin->isInstalled('tag') && $plugin->isActivated('tag')) {
-            if (isset($input['tag_questions'])) {
+         if (Plugin::isPluginActive('tag')) {
+            if (isset($input['_tag_questions'])) {
                $input['tag_questions'] = (!empty($input['_tag_questions']))
                                           ? implode(',', $input['_tag_questions'])
                                           : '';
             }
-            if (isset($input['tag_specifics'])) {
+            if (isset($input['_tag_specifics'])) {
                $input['tag_specifics'] = (!empty($input['_tag_specifics']))
                                           ? implode(',', $input['_tag_specifics'])
                                           : '';
