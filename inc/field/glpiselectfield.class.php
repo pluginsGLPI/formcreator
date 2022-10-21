e6986b040b8739e7eae77a33e28031bbf6223c25
Thierry Bugier <tbugier@teclib.com>
2022-10-21 12:48:12 +0200

    fix(glpiselectfield): comparison with regex

--- inc/field/glpiselectfield.class.php
+++ inc/field/glpiselectfield.class.php
@@ -243,10 +243,6 @@ class GlpiselectField extends DropdownField
       return !$this->greaterThan($value) && !$this->equals($value);
    }
 
-   public function regex($value): bool {
-      return (preg_grep($value, $this->value)) ? true : false;
-   }
-
    public function isAnonymousFormCompatible(): bool {
       return false;
    }
