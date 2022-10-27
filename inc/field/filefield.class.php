ebf0e020f66c3dd45336afb16d23b0b4f1db087b
Thierry Bugier <tbugier@teclib.com>
2022-10-27 15:00:46 +0200

    fix(filefield): do not assume index of files
    whern adding, removing then adding again a file, indexes are somewhat consumed. Indes 0 will not exist in uploads list.

--- inc/field/filefield.class.php
+++ inc/field/filefield.class.php
@@ -129,8 +129,7 @@ class FileField extends PluginFormcreatorAbstractField
          return;
       }
       $answer_value = [];
-      $index = 0;
-      foreach ($this->uploads["_$key"] as $document) {
+      foreach ($this->uploads["_$key"] as $index => $document) {
          $document = Toolbox::stripslashes_deep($document);
          if (is_file(GLPI_TMP_DIR . '/' . $document)) {
             $prefix = $this->uploads['_prefix_formcreator_field_' . $this->question->getID()][$index];
