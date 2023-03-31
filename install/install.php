8d08048101a295af0798644ed3ee28561212f221
Thierry Bugier <tbugier@teclib.com>
2023-03-31 10:54:53 +0200

    fix(targetchange,targetproblem): several fields must use rich text

--- install/install.php
+++ install/install.php
@@ -81,6 +81,7 @@ class PluginFormcreatorInstall {
       '2.13.1' => '2.13.3',
       '2.13.3' => '2.13.4',
       '2.13.4' => '2.13.5',
+      '2.13.5' => '2.13.6',
    ];
 
    protected bool $resyncIssues = false;
