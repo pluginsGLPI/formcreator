7df2f7a256b9b22c059b2ac3bc6d485f1c6f7469
Thierry Bugier <tbugier@teclib.com>
2023-04-17 15:36:40 +0200

    Revert "fix(radiosfield, selectfield): check default value before save"
    This reverts commit d4a2ecb4cff1b530c3970d4b7409adac39f45bb8.

--- inc/field/radiosfield.class.php
+++ inc/field/radiosfield.class.php
@@ -121,15 +121,6 @@ class RadiosField extends PluginFormcreatorAbstractField
       $input['values'] = $this->trimValue($input['values']);
       $input['default_values'] = trim($input['default_values']);
 
-      if (!$this->isValidValue($input['default_values'])) {
-         Session::addMessageAfterRedirect(
-            __('The default value is invalid:', 'formcreator') . ' ' . $input['name'],
-            false,
-            ERROR
-         );
-         return [];
-      }
-
       return $input;
    }
 
