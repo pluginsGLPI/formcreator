cf4d1fa02873e58d19ef73ec8bc66d0da18e2355
Thierry Bugier <tbugier@teclib.com>
2023-05-30 09:32:09 +0200

    fix: confitrmation strings

--- js/scripts.js
+++ js/scripts.js
@@ -1517,7 +1517,7 @@ var plugin_formcreator = new function() {
 // === TARGETS ===
 
 $(document).on('click', '.plugin_formcreator_duplicate_target', function() {
-   if(confirm(i18n.textdomain('formcreator').__('Are you sure you want to duplicate this target:', 'formcreator'))) {
+   if(confirm(i18n.textdomain('formcreator').__('Are you sure you want to duplicate this target?', 'formcreator'))) {
       $.post({
         url: formcreatorRootDoc + '/ajax/form_duplicate_target.php',
         data: {
@@ -1534,7 +1534,7 @@ $(document).on('click', '.plugin_formcreator_duplicate_target', function() {
 });
 
 $(document).on('click', '.plugin_formcreator_delete_target', function() {
-   if(confirm(i18n.textdomain('formcreator').__('Are you sure you want to delete this target:', 'formcreator'))) {
+   if(confirm(i18n.textdomain('formcreator').__('Are you sure you want to delete this target?', 'formcreator'))) {
       $.post({
         url: formcreatorRootDoc + '/ajax/form_delete_target.php',
         data: {
