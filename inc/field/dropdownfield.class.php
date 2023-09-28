b606c58cee837da0d208570ed088f41082d2d998
stonebuzz <skita@teclib.com>
2023-09-28 14:30:47 +0200

    feat(DropdownField): Load translation for plugin too

--- inc/field/dropdownfield.class.php
+++ inc/field/dropdownfield.class.php
@@ -715,6 +715,7 @@ class DropdownField extends PluginFormcreatorAbstractField
       // We need english locale to search searchOptions by name
       $oldLocale = $TRANSLATE->getLocale();
       $TRANSLATE->setLocale("en_GB");
+      $_SESSION['glpilanguage'] = "en_GB";
       if ($plug = isPluginItemType($itemtype)) {
          Plugin::loadLang(strtolower($plug['plugin']), "en_GB");
       }
@@ -736,6 +737,7 @@ class DropdownField extends PluginFormcreatorAbstractField
          if (count($searchOption) == 0) {
             trigger_error("No search option found for $property", E_USER_WARNING);
             $TRANSLATE->setLocale($oldLocale);
+            $_SESSION['glpilanguage'] = $oldLocale;
             return $content;
          }
 
@@ -779,6 +781,7 @@ class DropdownField extends PluginFormcreatorAbstractField
       }
       // Put the old locales on succes or if an expection was thrown
       $TRANSLATE->setLocale($oldLocale);
+      $_SESSION['glpilanguage'] = $oldLocale;
       if ($plug = isPluginItemType($itemtype)) {
          Plugin::loadLang(strtolower($plug['plugin']), $oldLocale);
       }
