09a5f1bc39c9b623dee7482fd710af2907ceb49e
Thierry Bugier <tbugier@teclib.com>
2023-10-03 13:53:26 +0200

    style(abstractfield): remote dead code

--- inc/abstractfield.class.php
+++ inc/abstractfield.class.php
@@ -201,7 +201,6 @@ abstract class PluginFormcreatorAbstractField implements PluginFormcreatorFieldI
       );
 
       return $value;
-      return $DB->escape(json_encode($value, JSON_UNESCAPED_UNICODE));
    }
 
    public function getFieldTypeName(): string {
