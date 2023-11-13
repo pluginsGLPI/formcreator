42b7efac12ea6642770aa3cdef26a0de7d4252cd
Thierry Bugier <tbugier@teclib.com>
2023-11-13 14:00:07 +0100

    fix(ldapfield): missing error message

--- inc/field/ldapselectfield.class.php
+++ inc/field/ldapselectfield.class.php
@@ -151,6 +151,7 @@ class LdapselectField extends SelectField
       if (isset($ldap_values['ldap_attribute'])) {
          $ldap_dropdown = RuleRightParameter::getById((int) $ldap_values['ldap_attribute']);
          if (!($ldap_dropdown instanceof RuleRightParameter)) {
+            Session::addMessageAfterRedirect(__('LDAP attribute is required!', 'formcreator'), false, ERROR);
             return [];
          }
       }
