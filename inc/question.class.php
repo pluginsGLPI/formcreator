8e06c17cd1b864f39a53e34e75a206acf3de66a5
Thierry Bugier <tbugier@teclib.com>
2024-03-13 12:34:28 +0100

    Revert "Revert "feat(glpiselectfield): support Cluster itemtype""
    This reverts commit a90c3e56e65352c23174ca1632fbd5b5a6a43499.

--- inc/question.class.php
+++ inc/question.class.php
@@ -1233,6 +1233,7 @@ PluginFormcreatorTranslatableInterface
             Line::class               => Line::getTypeName($plural),
             PassiveDCEquipment::class => PassiveDCEquipment::getTypeName($plural),
             PDU::class                => PDU::getTypeName($plural),
+            Cluster::class            => Cluster::getTypeName(2),
          ],
          __("Assistance") => [
             Ticket::class             => Ticket::getTypeName($plural),
