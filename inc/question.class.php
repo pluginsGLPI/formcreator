2d9a02aa45885bc23e35bdc61981d36ff4aca33d
MyuTsu <gaetan.langlois31@gmail.com>
2025-03-07 11:20:20 +0100

    fix: TU

--- inc/question.class.php
+++ inc/question.class.php
@@ -402,7 +402,7 @@ PluginFormcreatorTranslatableInterface
     * @return array the modified $input array
     */
    public function prepareInputForAdd($input) {
-      if (!empty($input['default_values'])) {
+      if (!in_array($input['fieldtype'], ['checkboxes', 'multiselect'])) {
          $input['default_values'] = str_replace('\r\n', '', $input['default_values']);
       }
 
@@ -456,7 +456,7 @@ PluginFormcreatorTranslatableInterface
    public function prepareInputForUpdate($input) {
       // global $DB;
 
-      if (!empty($input['default_values'])) {
+      if (!in_array($input['fieldtype'], ['checkboxes', 'multiselect'])) {
          $input['default_values'] = str_replace('\r\n', '', $input['default_values']);
       }
       if (!$this->skipChecks) {
