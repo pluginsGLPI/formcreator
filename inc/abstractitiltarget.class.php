pluginsGLPI/formcreator

commit 29ed099cfa7cdbd1f18bbd2bfb24528f444a96a7

Not imported template (#3449)
fix(targetticket,targetchange,targetproblem): not imported template

inc/abstracttarget.class.php
@@ -79,6 +79,8 @@ abstract class PluginFormcreatorAbstractTarget extends CommonDBChild implements
     */
    abstract protected function getTaggableFields();
 
+   abstract protected function getTemplateItemtypeName();
+
    const DESTINATION_ENTITY_CURRENT = 1;
    const DESTINATION_ENTITY_REQUESTER = 2;
    const DESTINATION_ENTITY_REQUESTER_DYN_FIRST = 3;
@@ -560,5 +562,19 @@ abstract class PluginFormcreatorAbstractTarget extends CommonDBChild implements
    public function prepareInputForClone($input) {
       unset($input['uuid']);
       return $input;
+   }
+
+   protected static function getTemplateByName(string $name): int {
+      $targetTemplateType = (new static())->getTemplateItemtypeName();
+      $targetTemplate = new $targetTemplateType();
+      $targetTemplate->getFromDBByCrit([
+         'name' => $name,
+      ]);
+
+      if ($targetTemplate->isNewItem()) {
+         return 0;
+      }
+
+      return $targetTemplate->getID();
    }
 }

inc/targetchange.class.php
@@ -288,6 +288,16 @@ class PluginFormcreatorTargetChange extends PluginFormcreatorAbstractItilTarget
          $input[$fieldSetting['field']] = $question->getID();
       }
 
+      // Find template by name
+      $input['changetemplates_id'] = 0;
+      if (is_string($input['_changetemplate']) && strlen($input['_changetemplate']) > 0) {
+         $input['changetemplates_id'] = self::getTemplateByName($input['_changetemplate']);
+         if ($input['changetemplates_id'] === 0) {
+            $typeName = strtolower(self::getTypeName());
+            throw new ImportFailureException(sprintf(__('Failed to add or update the %1$s %2$s: It uses a non existent template', 'formceator'), $typeName, $input['name']));
+         }
+      }
+
       // Add or update
       $originalId = $input[$idKey];
       $item->skipChecks = true;

inc/targetproblem.class.php
@@ -525,6 +525,15 @@ class PluginFormcreatorTargetProblem extends PluginFormcreatorAbstractItilTarget
          $input[$fieldSetting['field']] = $question->getID();
       }
 
+      // Find template by name
+      $input['problemtemplates_id'] = 0;
+      if (is_string($input['_problemtemplate']) && strlen($input['_problemtemplate']) > 0) {
+         $input['problemtemplates_id'] = self::getTemplateByName($input['_problemtemplate']);
+         if ($input['problemtemplates_id'] === 0) {
+            $typeName = strtolower(self::getTypeName());
+            throw new ImportFailureException(sprintf(__('Failed to add or update the %1$s %2$s: It uses a non existent template', 'formceator'), $typeName, $input['name']));
+         }
+      }
       // Add or update
       $originalId = $input[$idKey];
       $item->skipChecks = true;

inc/targetticket.class.php
@@ -1404,6 +1404,16 @@ class PluginFormcreatorTargetTicket extends PluginFormcreatorAbstractItilTarget
          $input[$fieldSetting['field']] = $question->getID();
       }
 
+      // Find template by name
+      $input['tickettemplates_id'] = 0;
+      if (is_string($input['_tickettemplate']) && strlen($input['_tickettemplate']) > 0) {
+         $input['tickettemplates_id'] = self::getTemplateByName($input['_tickettemplate'] ?? '');
+         if ($input['tickettemplates_id'] === 0) {
+            $typeName = strtolower(self::getTypeName());
+            throw new ImportFailureException(sprintf(__('Failed to add or update the %1$s %2$s: It uses a non existent template', 'formceator'), $typeName, $input['name']));
+         }
+      }
+
       // Add or update
       $originalId = $input[$idKey];
       $item->skipChecks = true;

tests/3-unit/PluginFormcreatorTargetChange.php
@@ -505,6 +505,7 @@ class PluginFormcreatorTargetChange extends AbstractItilTargetTestCase {
          'tag_specifics' => '',
          'category_rule' => \PluginFormcreatorTargetChange::CATEGORY_RULE_NONE,
          'category_question' => '0',
+         '_changetemplate' => '',
          'uuid' => $uuid,
       ];
 

tests/3-unit/PluginFormcreatorTargetTicket.php
@@ -782,6 +782,7 @@ class PluginFormcreatorTargetTicket extends AbstractItilTargetTestCase {
          'source_question' => 0,
          'type_rule' => 1,
          'type_question' => 0,
+         '_tickettemplate' => '',
          'uuid' => $uuid,
       ];
 

commit 361cba31da12ea8a7860c8ed2cc1724e98a8ebe2

Fix breaking change introduced by #3449

inc/abstractitiltarget.class.php
@@ -2539,4 +2539,20 @@ SCRIPT;
       $input['_skip_create_actors'] = true;
       return $input;
    }
+
+   protected static function getTemplateByName(string $name): int {
+      global $DB;
+
+      $targetTemplateType = (new static())->getTemplateItemtypeName();
+      $targetTemplate = new $targetTemplateType();
+      $targetTemplate->getFromDBByCrit([
+         'name' => $DB->escape($name),
+      ]);
+
+      if ($targetTemplate->isNewItem()) {
+         return 0;
+      }
+
+      return $targetTemplate->getID();
+   }
 }

inc/abstracttarget.class.php
@@ -79,8 +79,6 @@ abstract class PluginFormcreatorAbstractTarget extends CommonDBChild implements
     */
    abstract protected function getTaggableFields();
 
-   abstract protected function getTemplateItemtypeName();
-
    const DESTINATION_ENTITY_CURRENT = 1;
    const DESTINATION_ENTITY_REQUESTER = 2;
    const DESTINATION_ENTITY_REQUESTER_DYN_FIRST = 3;
@@ -573,19 +571,4 @@ abstract class PluginFormcreatorAbstractTarget extends CommonDBChild implements
       return $input;
    }
 
-   protected static function getTemplateByName(string $name): int {
-      global $DB;
-
-      $targetTemplateType = (new static())->getTemplateItemtypeName();
-      $targetTemplate = new $targetTemplateType();
-      $targetTemplate->getFromDBByCrit([
-         'name' => $DB->escape($name),
-      ]);
-
-      if ($targetTemplate->isNewItem()) {
-         return 0;
-      }
-
-      return $targetTemplate->getID();
-   }
 }